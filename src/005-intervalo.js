f8757afe7995b79d24b757d83e821a4ff211cd41
LaksCastro <lakhsmycastro336@gmail.com>
2020-02-16 09:53:44 -0400

    rename 004 and add 005 code

---
+++ src/005-intervalo.js
@@ -0,0 +1,8 @@
+// Aqui é o ponte de entrada
+const entrada = Number(lines.shift());
+const foraDoIntervalo = entrada < 0 || entrada > 100;
+if (foraDoIntervalo) console.log("Fora de intervalo");
+else if (entrada >= 0 && entrada <= 25) console.log("Intervalo [0,25]");
+else if (entrada > 25 && entrada <= 50) console.log("Intervalo (25,50]");
+else if (entrada > 50 && entrada <= 75) console.log("Intervalo (50,75]");
+else console.log("Intervalo (75,100]");
