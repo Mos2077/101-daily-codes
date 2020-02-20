3aee4d1318ec11677e410a8711f934f9a8e91330
Laks Castro <lakhsmycastro336@gmail.com>
2020-02-20 08:20:00 -0400

    add(009): adding comments

--- src/009-teste-selecao.js
+++ src/009-teste-selecao.js
@@ -12,14 +12,35 @@
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
+// Ponto de entrada dos valores pedidos no exercício aqui
+// Separe cada valor por um espaço
 const entrada = "5 6 7 8";
+
+// Pegamos a string e pegamos o valor A, B, C e D
 const [A, B, C, D] = entrada.split(" ").map(item => Number(item));
+
+// Criamos variáveis que vão armazenar true se a condição
+// Do exercício for atendida
+
+// Armazena true se o valor B for maior que A
 const BMaiorC = B > C;
 const DMaiorA = D > A;
+
+// Armazena true se a soma de C e D for maior que a soma de A e B
 const SomaCDMaiorSomaAB = C + D > A + B;
+
+// Armazena true se A for par
+// O operador "%" pega o resto da divisão
+// Ou seja, se o resto da divisão de A por 2 é 0, então A é par
 const APar = A % 2 === 0;
+
+// Caso todas as variáveis acima forem true/verdadeiras e mais estas:
+// 1. C for positivo
+// 2. D for positivo
 if(BMaiorC && DMaiorA && SomaCDMaiorSomaAB && C >= 0 && D >= 0){
+  // Se cair aqui, então os valores foram aceitos, então mostramos no console
   console.log("Valores aceitos");
 }else{
+  // Se cair aqui, então alguma verificação deu falso, então não aceitamos os valores
   console.log("Valores nao aceitos")
 }
