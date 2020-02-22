f1c9d7540813ef51351b2b2cc0f36b6fc39ea8ac
Laks Castro <lakhsmycastro336@gmail.com>
2020-02-22 01:25:56 -0400

    Create 011-numeros-positivos.js

---
+++ src/011-numeros-positivos.js
@@ -0,0 +1,15 @@
+// EXERÍCIO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+// URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/1060
+
+// Faça um programa que leia 6 valores. 
+// Estes valores serão somente negativos ou positivos (desconsidere os valores nulos). 
+// A seguir, mostre a quantidade de valores positivos digitados.
+
+// Entrada
+// Seis valores, negativos e/ou positivos.
+
+// Saída
+// Imprima uma mensagem dizendo quantos valores positivos foram lidos.
+
+// CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
