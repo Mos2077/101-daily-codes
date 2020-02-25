07584a2e35a5dfb91b594256dea426951c1e56b1
Laks Castro <lakhsmycastro336@gmail.com>
2020-02-25 06:49:40 -0400

    Update 014-led.js

--- src/014-led.js
+++ src/014-led.js
@@ -15,3 +15,46 @@
 // seguido da palavra "leds".
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+// Definimos a quantidade de LED's para cada número, começando do zero
+const leds = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6];
+// Número 0 : precisa de 6 LED's
+// Número 1 : precisa de 2 LED's
+// etc...
+
+// Aqui lemos a quantidade de casos de teste que o usuário vai usar
+const caseTestLength = Number("1");
+
+// For que permite que executemos N casos de testes
+for (let i = 0; i < caseTestLength; i++) {
+  // Este bloco de código será executado para cada número que o usuário inserir
+  
+  // Iniciamos a quantidade de LED's com 0
+  let ledQuantity = 0;
+  
+  // Lemos o valor da entrada do usuário, neste exemplo coloquei o número "52"
+  const value = "52";
+  
+  // Dividimos a string da variavel anterior em um array
+  // Esse array contém cada caracter do numero, por exemplo:
+  // Valor da variavel value: "52"
+  // A variavel valueArray fica = ["5", "2"]
+  const valueArray = value.split("");
+
+  // For que vai percorrer cada caracter da variavel valueArray
+  for (let i = 0; i < valueArray.length; i++) {
+    // Pegamos o caracter atual do laço de repetição
+    const item = Number(valueArray[i]);
+    
+    // Pegamos a quantidade de LED's necessárias usando de base o item como indice do array e adicionamos na variavel ledQuantity
+    // Nota: += é equivalente a escrever:
+    // ledQuantity = ledQuantity + leds[item]
+    // Ou seja, estamos adicionando o valor que já tem mais o valor do array leds
+    ledQuantity += leds[item];
+  }
+  
+  // Mostramos no console
+  console.log(`${ledQuantity} leds`);
+  
+  // O laço continua ou para se já for o ultimo caso de teste
+}
