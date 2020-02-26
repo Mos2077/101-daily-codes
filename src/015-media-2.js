ad0a894f6edff2cd8a97f9adce69d8d360eec5dc
Laks Castro <lakhsmycastro336@gmail.com>
2020-02-26 09:57:14 -0400

    Update 015-media-2.js

--- src/015-media-2.js
+++ src/015-media-2.js
@@ -2,7 +2,6 @@
 
 // URL:
 
-
 // Leia 3 valores, no caso, variáveis A, B e C, que são as três notas de um aluno. 
 // A seguir, calcule a média do aluno, sabendo que a nota A tem peso 2, a nota B tem peso 3 e a nota C tem peso 5. 
 // Considere que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
@@ -16,22 +15,30 @@
 // Assim como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, 
 // você receberá "Presentation Error".
 
+
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
-const a = Number(lines.shift()),
-  b = Number(lines.shift()),
-  c = Number(lines.shift());
+// Aqui é o ponto de entrada para a nota A, B e C do aluno
+// Cada nota com pesos 2, 3, 5, respectivamente
+const a = 10.0,
+  b = 4.8,
+  c = 8.5;
 
+// Primeiro vamos multiplicar cada nota por seu peso
 const pesoA = a * 2,
   pesoB = b * 3,
   pesoC = c * 5;
 
+// Em seguida vamos somar todas os resultados anteriores
+// E também somar os pesos e guardar na variavel divisor
 const dividendo = pesoA + pesoB + pesoC,
-  divisor = 10;
+  divisor = 2 + 3 + 5;
 
+// Calculamos a media
 const media = dividendo / divisor;
 
+// Criamos uma mensagem
 const message = "MEDIA = " + media.toFixed(1);
 
+// Mostramos a mensagem no console
 console.log(message);
-
