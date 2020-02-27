d8e13fe356d02eb070c1a6fec5017cfbb8c4db7d
Laks Castro <lakhsmycastro336@gmail.com>
2020-02-27 07:25:35 -0400

    Update 016-consumo.js

--- src/016-consumo.js
+++ src/016-consumo.js
@@ -16,8 +16,18 @@
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
-const litros = Number(lines.shift());
-const distancia = Number(lines.shift());
+// Valor de entrada para a quantidade de litros de uma corrida
+const litros = 500;
+
+// Valor de entrada para a distancia que foi percorrida nessa corrida
+const distancia = 35;
+
+// Calculamos a média dividindo a quantidade de listros pela distância
 const media = litros / distancia;
+
+// Geramos a mensagem baseado na média que calculamos anteriormente
+// Nota: toFixed(3) para obrigar que a média tenha 3 casa decimais
 const message = `${media.toFixed(3)} km/l`;
+
+// Mostramos no console a mensagem gerada anteriormente
 console.log(message);
