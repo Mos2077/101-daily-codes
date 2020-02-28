bc895747b6f37fa1cb182a552c522d7aefbad7f0
Laks Castro <lakhsmycastro336@gmail.com>
2020-02-28 08:14:32 -0400

    Update 017-problemas-com-calculadora.js

--- src/017-problemas-com-calculadora.js
+++ src/017-problemas-com-calculadora.js
@@ -16,11 +16,29 @@
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
-const casosTeste = Number("3");
+// Aqui lemos a quantidade de casos de testes que devem ser feito
+const casosTeste = 5;
+
+// For que permite a iteração, repete o código de código X vezes, o valor de X é a quantidade de casos de testes que foram lidas acima
 for (let i = 0; i < casosTeste; i++) {
+  // Primeiro lemos a entrada da string que vamos retirar os numeros
   const entrada = "Ab23s249ttu21";
+
+  // Antes de continuar, vamos ver como funciona o método substring:
+  // String de exemplo: "CARRO"
+  // "CARRO".substring(0, 1) vai retonar C
+  // Pois: o primeiro parâmetro é o index da string que queremos retornar, já o segundo é o index da string que queremos cortar a string (não incluindo)
+
+  // Nota: use o Number() para transformar a String em um número
+  // Pegamos o primeiro número
   const one = Number(entrada.substring(2, 4));
+
+  // Pegamos o segundo número
   const two = Number(entrada.substring(5, 8));
+
+  // E pegamos o terceiro número
   const three = Number(entrada.substring(11, 13));
+  
+  // E mostramos a soma no console
   console.log(one + two + three);
 }
