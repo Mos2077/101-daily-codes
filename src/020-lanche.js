62b7c6ebc6c6d6d2d8a5a60eac8c806306bd01f6
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-02 17:02:26 -0400

    Update 020-lanche.js

--- src/020-lanche.js
+++ src/020-lanche.js
@@ -15,29 +15,38 @@
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
-const input = require('fs').readFileSync('/dev/stdin', 'utf8');
-const lines = input.split('\n');
-const [id, qtd] = lines.shift().split(" ").map(item => Number(item));
+// Primeiro elemento é o ID, e o segundo elemento é a quantidade
+const [id, qtd] = [3, 2];
 
+// Variável com o preço do produto seleciondo, começamos com 0, pois ainda não sabemos qual elemento é
 let price = 0;
 
+// Fazemos um switch pro ID da comida (ID = código)
 switch(id){
   case 1: 
+    // Se o ID for 1, o preço será R$ 4
     price = 4;
   break;
   case 2: 
+    // Se o ID for 2, o preço será R$ 4,50
     price = 4.5;
   break;
   case 3: 
+    // Se o ID for 3, o preço será R$ 5,00
     price = 5;
   break;
   case 4: 
+    // Se o ID for 4, o preço será R$ 2,00
    price = 2;
   break;
   case 5: 
+    // Se o ID for 1, o preço será R$ 1,50
     price = 1.5;
   break;
 }
 
+// Aqui calculamos o total multiplicando o preço do produto pela quantidade
 const total = price * qtd; 
+
+// Mostramos no console
 console.log(`Total: R$ ${total.toFixed(2)}`); 
