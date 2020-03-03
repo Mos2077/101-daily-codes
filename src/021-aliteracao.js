21fd47769add3c5e764addb23c05bf46c26a8986
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-03 12:41:57 -0300

    Update 021-aliteracao.js

--- src/021-aliteracao.js
+++ src/021-aliteracao.js
@@ -16,3 +16,35 @@
 
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+let getout = false;
+while (!getout) {
+  const text = lines.shift().toLowerCase();
+  if (!text) {
+    getout = true;
+    break;
+  }
+  const words = text.split(" ");
+
+  let alliterations = 0;
+  let lastLetter = words[0].charAt(0);
+  let lastAlliteration = null;
+
+  for (let i = 1; i < words.length; i++) {
+    const word = words[i];
+    const currentLetter = word.charAt(0);
+
+    if (lastLetter === currentLetter) {
+      if (currentLetter !== lastAlliteration) {
+        alliterations++;
+        lastAlliteration = currentLetter;
+      }
+      lastLetter = currentLetter;
+    } else {
+      lastLetter = currentLetter;
+      lastAlliteration = null;
+    }
+  }
+
+  console.log(alliterations);
+}
