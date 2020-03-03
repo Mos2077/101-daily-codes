c5b6bc2e84515251b279263157cb4414320b7c71
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-03 12:41:44 -0300

    Update 021-aliteracao.js

--- src/021-aliteracao.js
+++ src/021-aliteracao.js
@@ -1,5 +1,18 @@
-// EXERCÍCIO 
+// EXERCÍCIO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
 // URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/1263
 
-// CÓDIGO 
+// Uma aliteração ocorre quando duas ou mais palavras consecutivas de um texto possuem a mesma letra inicial 
+// (ignorando maiúsculas e minúsculas). Sua tarefa é desenvolver um programa que identifique, 
+// a partir de uma sequência de palavras, o número de aliterações que essa sequência possui.
+
+// Entrada
+// A entrada contém diversos casos de testes. Cada caso é expresso como um texto em uma única linha, 
+// contendo de 1 a 100 palavras separadas por um único espaço, 
+// cada palavra tendo de 1 a 50 letras minúsculas ou maiúsculas ('A'-'Z','a'-'z'). A entrada termina em EOF.
+
+// Saída
+// Para cada caso de teste imprima o número de aliterações existentes no texto informado, conforme exemplos abaixo.
+
+
+// CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
