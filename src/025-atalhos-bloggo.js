7b90b1125bf570b19b64974563a44ca9efd803d8
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-07 15:13:25 -0300

    Update 025-atalhos-bloggo.js

--- src/025-atalhos-bloggo.js
+++ src/025-atalhos-bloggo.js
@@ -4,3 +4,29 @@
 
 // CÓDIGO
 
+let string = lines.shift();
+
+const letters = string.split("");
+const formattedLetters = letters;
+
+let searchingItalicEndTag = false;
+let searchingBoldEndTag = false;
+
+for (let i = 0; i < letters.length; i++) {
+  const letter = letters[i];
+
+  const initItalicTag = "<i>";
+  const endItalicTag = "</i>";
+
+  const initBoldTag = "<b>";
+  const endBoldTag = "</b>";
+
+  if (letter === "_") {
+    formattedLetters[i] = searchingItalicEndTag ? endItalicTag : initItalicTag;
+    searchingItalicEndTag = !searchingItalicEndTag;
+  } else if (letter === "*") {
+    formattedLetters[i] = searchingBoldEndTag ? endBoldTag : initBoldTag;
+    searchingBoldEndTag = !searchingBoldEndTag;
+  }
+}
+console.log(formattedLetters.join(""));
