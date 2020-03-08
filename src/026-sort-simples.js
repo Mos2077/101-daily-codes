5bfffe62d6595b6aa984496e8a5d9b4556374880
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-08 08:54:03 -0400

    Update 026-sort-simples.js

--- src/026-sort-simples.js
+++ src/026-sort-simples.js
@@ -2,4 +2,16 @@
 
 // URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/1042
 
+// Leia 3 valores inteiros e ordene-os em ordem crescente. 
+// No final, mostre os valores em ordem crescente, uma linha em branco e 
+// em seguida, os valores na sequência como foram lidos.
+
+// Entrada
+// A entrada contem três números inteiros.
+
+// Saída
+// Imprima a saída conforme foi especificado.
+
+
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
