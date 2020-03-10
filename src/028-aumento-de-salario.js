084ec319ff10ac2ab9707eae4d749cc32097b3ba
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-10 08:34:04 -0400

    Update 028-aumento-de-salario.js

--- src/028-aumento-de-salario.js
+++ src/028-aumento-de-salario.js
@@ -1,3 +1,5 @@
+// EXERCÍCIOS <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
 
 // A empresa ABC resolveu conceder um aumento de salários a seus funcionários de acordo com a tabela abaixo:
 
@@ -22,3 +24,28 @@
 
 // Saída
 // Imprima 3 linhas na saída: o novo salário, o valor ganho de reajuste e o percentual de reajuste ganho, conforme exemplo abaixo.
+
+
+// CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+const salary = Number(lines.shift());
+
+let percent;
+
+if (salary <= 400) {
+  percent = 15;
+} else if (salary > 400 && salary <= 800) {
+  percent = 12;
+} else if (salary > 800 && salary <= 1200) {
+  percent = 10;
+} else if (salary > 1200 && salary <= 2000) {
+  percent = 7;
+} else {
+  percent = 4;
+}
+
+const reajust = salary * (percent / 100);
+const newSalary = salary + reajust;
+console.log("Novo salario: " + newSalary.toFixed(2));
+console.log("Reajuste ganho: " + reajust.toFixed(2));
+console.log("Em percentual: " + percent + " %");
