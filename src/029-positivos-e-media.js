7814bdddbe226502037f2d32de6bb66286e148ff
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-11 08:31:10 -0400

    Update 029-positivos-e-media.js

--- src/029-positivos-e-media.js
+++ src/029-positivos-e-media.js
@@ -16,21 +16,40 @@
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
+// Array de números de tamanho fixo (6) 
+// Pegaremos os valores positivos e faremos média entre os valores positivos
 let numbers = [6, 9, 7, 2, 9, 1];
+
+// Array que vai armazenas os valores positivos
+// Basicamente vamos copiar os elementos positivos fo array anterior para este
 let positives = [];
+
+// Variável que armazena a soma dos valores positivos
 let sum = 0;
 
+// For que vai percorrer cada elemento do array numbers
 for(let i = 0; i < numbers.length; i++){
+  // Pegamos o número atual usando como base o index (i) do for
   const num = numbers[i];
+
+  // Se esse número for maior ou igual a zero, vamos adicionar ao array de números
+  // E somar à variável sum
   if(num >= 0){
+    // Nota: é o push() que adiciona o elemento ao array positives
     positives.push(num);
+    // Short sintaxe para sum = sum + num
     sum += num;
   }
 }
 
+// Aqui calcularemos a média, ou seja, pegamos a soma dos elementos positivos
+// E dividimos pela quantidade de números positivos (o positives.length)
 const average = sum / positives.length;
 
 
+// Mostramos no console a quantidade de valores positivos
 console.log(positives.length + " valores positivos");
 
+// Mostramos no console a média dedses valores positivos com uma casa decimal
+// Nota: é o toFixed() que permite formatar o número com uma casa decimal
 console.log(average.toFixed(1));
