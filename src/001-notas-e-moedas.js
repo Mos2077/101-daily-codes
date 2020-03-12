de577d7aef0ba681cae8243c33c4a27ee352140d
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-12 08:00:03 -0400

    Update 001-notas-e-moedas.js

--- src/001-notas-e-moedas.js
+++ src/001-notas-e-moedas.js
@@ -95,3 +95,20 @@ console.log("MOEDAS:");
 trocoMoedas.forEach((item, i) => {
   console.log(`${item} moeda(s) de R$ ${moedas[i].toFixed(2)}`);
 });
+
+
+// Saída esperada com a entrada do usuário igual a 167.50
+// 'NOTAS:'
+// '1 nota(s) de R$ 100.00'
+// '1 nota(s) de R$ 50.00'
+// '0 nota(s) de R$ 20.00'
+// '1 nota(s) de R$ 10.00'
+// '1 nota(s) de R$ 5.00'
+// '1 nota(s) de R$ 2.00'
+// 'MOEDAS:'
+// '0 moeda(s) de R$ 1.00'
+// '1 moeda(s) de R$ 0.50'
+// '0 moeda(s) de R$ 0.25'
+// '0 moeda(s) de R$ 0.10'
+// '0 moeda(s) de R$ 0.05'
+// '0 moeda(s) de R$ 0.01'
