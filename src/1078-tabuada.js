26caf3a20730197a057141a7702485a87a310743
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-14 13:08:36 -0400

    Update 1078-tabuada.js

--- src/1078-tabuada.js
+++ src/1078-tabuada.js
@@ -29,3 +29,16 @@ for (let i = 1; i <= 10; i++) {
   // E colocamos essa mensagem para ser exibida no console a cada repetição
   console.log(i + " x " + num + " = " + i * num);
 }
+
+// Saída esperada para N (variável num) igual a 5
+// '1 x 5 = 5'
+// '2 x 5 = 10'
+// '3 x 5 = 15'
+// '4 x 5 = 20'
+// '5 x 5 = 25'
+// '6 x 5 = 30'
+// '7 x 5 = 35'
+// '8 x 5 = 40'
+// '9 x 5 = 45'
+// '10 x 5 = 50'
+
