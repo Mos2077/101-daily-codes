59595846f28437afcc54b86df1039c1ac9f3e1be
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-19 02:40:05 -0300

    Update 037-sequencia-de-numeros-e-soma.js

--- src/037-sequencia-de-numeros-e-soma.js
+++ src/037-sequencia-de-numeros-e-soma.js
@@ -1,1 +1,13 @@
 
+let min = 20;
+let max = 25;
+
+let sum = 0;
+let string = "";
+
+for (; min <= max; min++) {
+  sum += min;
+  string += !string.length ? min : " " + min;
+}
+
+console.log(string + " Sum=" + sum);
