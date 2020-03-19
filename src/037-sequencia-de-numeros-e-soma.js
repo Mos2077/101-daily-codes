dc5eee6775a002a8aac6df4e225347173ee25318
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-19 02:48:06 -0300

    Update 037-sequencia-de-numeros-e-soma.js

--- src/037-sequencia-de-numeros-e-soma.js
+++ src/037-sequencia-de-numeros-e-soma.js
@@ -13,15 +13,28 @@
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
+// Var que armazena o menor valor, o valor de onde iniciaremos a contagem e a soma
 let min = 20;
+
+// Var que armazena o último valor da contagem, é o ultimo valor que adicionaremos à soma
 let max = 25;
 
+// Variável que vai armazenar a soma de todos os numeros inteiros (naturais) que estão entre min a max, incluindo eles
 let sum = 0;
+
+// Essa é simplesmente a variável que vai armazenar uma string com todos os numeros em fileira separados por um espaço
 let string = "";
 
+// Vamos repetir esse laço até que a variável min seja igual a variavel max, e a cada repetição, adicionaremos 1 à min, afinal, queremos apenas os numeros inteiros
 for (; min <= max; min++) {
+  // Em cada repetição, somamos min à variavel sum, para que no final, a variavel sum esteja com a soma de todas as repetições
   sum += min;
+  
+  // Aqui, é feita basicamente a seguinte pergunta:
+  // A vaiavel string está vazia? Se estiver, quer dizer que estamos na primeira repetição, então adicione á string somente o número, mais nada. Porém se a variável string não estiver vazia, então quer dizer que já há números nela, nesse caso, adicione mais um valor (a variavel min também), porém com um espaço na frente para separar do numero anterior
   string += !string.length ? min : " " + min;
 }
 
+// Mostramos no console a string da fileira de numeros e juntamos ao valor da soma
 console.log(string + " Sum=" + sum);
+
