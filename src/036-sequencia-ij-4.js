e14312c08a677fa138bca4aef466eff3af21f6a5
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-18 21:05:03 -0300

    Update 036-sequencia-ij-4.js

--- src/036-sequencia-ij-4.js
+++ src/036-sequencia-ij-4.js
@@ -1,3 +1,15 @@
+// EXERCÍCIO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+// URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/1098
+
+// Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+
+// Entrada
+// Não há nenhuma entrada neste problema.
+
+// Saída
+// Imprima a sequencia conforme exemplo abaixo.
+
 
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
