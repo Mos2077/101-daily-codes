70a575fb5c43bb81250dc9041306a74d96d5f1bd
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-18 21:01:32 -0300

    Update 036-sequencia-ij-4.js

--- src/036-sequencia-ij-4.js
+++ src/036-sequencia-ij-4.js
@@ -1,6 +1,18 @@
+// Vamos criar um for que vai:
+// Criar uma variável chamada i que vai começar por 0
+// Essa variável vai ganhar 0.2 a cada repetição
+// Vamos continuar repetindo enquanto essa variável for menor ou igual a 2
 for (let i = 0; i <= 2; i += 0.2) {
-  i = Number(i.toFixed(1));
+  // O JavaScript por alguma falha na Matrix ou conspiração alienígena, ele ao invés de fazer isso: 0.4 + 0.2 = 0.6, ele faz isto: 0.4 + 0.2 = 0.6000000000000001
+  // Então para obrigar ele a dar apenas uma casa decimal, nós primeiro usamos o .toFixed(1) e em seguida usamos o Number() para transformar a string com uma casa decimal em um número de verdade
+    i = Number(i.toFixed(1));
+  
+  // Para cada loop da variável i, nós criamos outro loop, que irá fazer o seguinte:
+  // Criar uma variável de nome j = que vai começar com o valor da variável i + 1
+  // J vai ganhar 1 a cada repetição
+  // Vamos continuar repetindo enquanto a variável J for menor que i + 3. Pois essa verificação vai permitir que o laço se repita apenas 3x para cada repetição do laço da variável i
   for (let j = i + 1; j <= i + 3; j++) {
+    // E dentro deste for, nós printamos a variável I e J na tela, como pede o exercício
     console.log("I=" + i + " J=" + j);
   }
 }
