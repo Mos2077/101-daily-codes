a1562bd062cdb1fdf371b73b64de50df4265d2f6
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-21 15:33:29 -0300

    Update 039-dividindo-x-por-y.js

--- src/039-dividindo-x-por-y.js
+++ src/039-dividindo-x-por-y.js
@@ -15,12 +15,23 @@
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
+// Este é a string com os valores de entrada:
+// Vamos dividir o primeiro número pelo segundo
 const string = "10 6";
 
+// Primeiro é necessário dividir essa string em dois números, para isso, vou utilizar o método "split" e "map"
+// 1. O método "split" divide uma string, e transforma ela em um array, e é o que faremos, usando como base para a divisão da string um espaço vazio " "
+// 2. Com o array retornado pelo método split, nós usamos o map() para percorrer os 2 valores desse array (que foi criado pelo split), mas para que percorrer? Para torná-los um número, usando o método Number()
+// Logo, ao final dos métodos, nós desestruturamos o array criado que contém os 2 números, pegando o valor "a" e "b"
 const [a, b] = string.split(" ").map(item => Number(item));
 
+// Caso "b", ou seja, o segundo valor, seja igual a zero, então a divisão não pode ocorrer, pois é impossível efetuar uma divisão por zero
 if (b === 0) console.log("divisao impossivel");
+
+// Caso "b" não seja igual a zero, então basta dividir a por b e usar o .toFixed() para obrigar que a divisão tenha pelos menos uma casa decimal
+// Nota: a quantidade de casas decimais é definida pelo valor que passamos como parâmetro para o toFixed();
 else console.log((a / b).toFixed(1));
+
 
 // Saída esperada para a entrada como os números (em string) "10 6":
 // '1.7'
