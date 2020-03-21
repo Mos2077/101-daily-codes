be8db1968f3e39f656b8fabafd4fd193b5e9bee7
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-21 15:27:05 -0300

    Update 039-dividindo-x-por-y.js

--- src/039-dividindo-x-por-y.js
+++ src/039-dividindo-x-por-y.js
@@ -11,3 +11,13 @@
 // Para cada caso mostre o resultado da divisão com um dígito após o ponto decimal, ou “divisao impossivel” caso não seja possível efetuar o cálculo.
 
 // Obs.: Cuide que a divisão entre dois inteiros em algumas linguagens como o C e C++ gera outro inteiro. Utilize cast :)
+
+
+// CÓDIGO <><><><><><><><><><><><><><><>
+
+const string = "10 6";
+
+const [a, b] = string.split(" ").map(item => Number(item));
+
+if (b === 0) console.log("divisao impossivel");
+else console.log((a / b).toFixed(1));
