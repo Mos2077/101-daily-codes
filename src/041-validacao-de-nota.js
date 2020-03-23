ad73b0cb4ee702a77bd9f829b6fd96145f8b33b9
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-23 18:27:31 -0300

    Update 041-validacao-de-nota.js

--- src/041-validacao-de-nota.js
+++ src/041-validacao-de-nota.js
@@ -11,3 +11,19 @@
 // Se uma nota inválida  for lida, deve ser impressa a mensagem "nota invalida".
 // Quando duas notas válidas forem lidas, deve ser impressa a mensagem "media = " seguido do valor do cálculo. 
 // O valor deve ser apresentado com duas casas após o ponto decimal.
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+const numbers = [];
+
+let validNumbers = numbers.length === 2;
+
+while(!validNumbers){
+    const num = Number(lines.shift());
+    if(num >= 0 && num <= 10) numbers.push(num);
+    else console.log("nota invalida");
+    validNumbers = numbers.length === 2;
+}
+
+console.log("media = " + ((numbers[0] + numbers[1]) / 2));
