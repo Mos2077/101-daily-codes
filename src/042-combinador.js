ab2a0397f82fa98bbd413bcc39b23a66275b8042
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-24 16:41:54 -0300

    Update 042-combinador.js

--- src/042-combinador.js
+++ src/042-combinador.js
@@ -22,22 +22,34 @@
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
+// Ponto de entrada, essas são as duas strings (A e B) que iremos combinar
 const stringA = "ab";
 const stringB = "cd";
 
+// Primeiro precisamos saber qual o tamanho da maior string na qual estamos combinando, neste caso, as duas são do mesmo tamanho ebtão não há diferença, porém em outros casos de teste, isso é necessário
 const lastIndex =
-  stringA.length > stringB.length ? stringA.length : stringB.length;
+   > stringB.length ? stringA.length : stringB.length;
 
-const finalString = [];
+// Aqui iremos armazenar a string final, a string combinada
+let finalString = "";
 
+// Aqui vamos percorrer X vezes, onde X é o tamanho da maior das duas strings que estamos combinando
 for (let i = 0; i < lastIndex; i++) {
+  // Aqui vamos pegar o valor atual da string A, "atual" baseada no indice atual
+  // Nota: usamos o "|| false" no final por que é possível que a string A seja menor que a string B ou vice versa, isso faz com que, em alguma repetição, em algum caso de teste, seja possível que o valor atual da string A ou B seja undefined, null. E nós não queremos adicionar isso à nossa string final, por isso, essa linha é importante: ela permite armazenar na variável "currentA" o valor da letra atual da string (se houver) ou o valor booleano falso, caso não houver
   const currentA = stringA[i] || false;
-  if (currentA) finalString.push(currentA);
+  
+  // Se o valor existir, adicione à variável string finalString, por isso fizemos aquela vericação anteriormente
+  if (currentA) finalString += currentA;
 
+  // Fazemos exatamente a mesma coisa, só que com a string B
   const currentB = stringB[i] || false;
-  if (currentB) finalString.push(currentB);
+  if (currentB) finalString += currentB;
 }
-console.log(finalString.join(""));
+
+// Ao final do laço, nossa string final está montada, basta exibir no console
+console.log(finalString);
+
 
 // Saída esperada para a combinação das duas strings: "ab" e "cd"
 // 'acbd'
