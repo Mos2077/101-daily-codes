0b5ffe80e2f7d258ddf74e7bc0a7ac4da5fd13bc
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-24 16:28:03 -0300

    Update 042-combinador.js

--- src/042-combinador.js
+++ src/042-combinador.js
@@ -16,3 +16,20 @@
 
 // Saída
 // Combine as duas cadeias de caracteres da entrada como mostrado no exemplo abaixo e exiba a cadeia resultante.
+
+const stringA = "ab";
+const stringB = "cd";
+
+const lastIndex =
+  stringA.length > stringB.length ? stringA.length : stringB.length;
+
+const finalString = [];
+
+for (let i = 0; i < lastIndex; i++) {
+  const currentA = stringA[i] || false;
+  if (currentA) finalString.push(currentA);
+
+  const currentB = stringB[i] || false;
+  if (currentB) finalString.push(currentB);
+}
+console.log(finalString.join(""));
