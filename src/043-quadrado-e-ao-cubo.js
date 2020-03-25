7b89671a4bf0126a97f00d7679a6a7ef06309543
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-25 19:41:41 -0300

    Update 043-quadrado-e-ao-cubo.js

--- src/043-quadrado-e-ao-cubo.js
+++ src/043-quadrado-e-ao-cubo.js
@@ -1,4 +1,12 @@
 
+// Escreva um programa que leia um valor inteiro N (1 < N < 1000). Este N é a quantidade de linhas de saída que serão apresentadas na execução do programa.
+
+// Entrada
+// O arquivo de entrada contém um número inteiro positivo N.
+
+// Saída
+// Imprima a saída conforme o exemplo fornecido.
+
 const num = Number("5");
 
 for (let i = 1; i <= num; i++) {
