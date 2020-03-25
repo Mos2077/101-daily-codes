291b226065f0a00015e1729aecec4b9ba1f10b5a
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-25 19:42:51 -0300

    Update 043-quadrado-e-ao-cubo.js

--- src/043-quadrado-e-ao-cubo.js
+++ src/043-quadrado-e-ao-cubo.js
@@ -1,6 +1,9 @@
 // EXERCÍCIO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
-// Escreva um programa que leia um valor inteiro N (1 < N < 1000). Este N é a quantidade de linhas de saída que serão apresentadas na execução do programa.
+// URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/1143
+
+// Escreva um programa que leia um valor inteiro N (1 < N < 1000). 
+// Este N é a quantidade de linhas de saída que serão apresentadas na execução do programa.
 
 // Entrada
 // O arquivo de entrada contém um número inteiro positivo N.
