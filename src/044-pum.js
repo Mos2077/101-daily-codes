393e4039e89e38eb5c6b59be279be466e959420c
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-26 02:16:08 -0300

    Update 044-pum.js

--- src/044-pum.js
+++ src/044-pum.js
@@ -18,3 +18,12 @@ const num = 7;
 for (let i = 1; i <= num * 4; i += 4) {
   console.log(`${i} ${i + 1} ${i + 2} PUM`);
 }
+
+// Saída esperada para a quantidade de linhas igual a 7:
+// '1 2 3 PUM'
+// '5 6 7 PUM'
+// '9 10 11 PUM'
+// '13 14 15 PUM'
+// '17 18 19 PUM'
+// '21 22 23 PUM'
+// '25 26 27 PUM'
