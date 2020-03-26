061adf97010b334ac0d311cbcfc44f02c741e65a
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-26 02:21:42 -0300

    Update 044-pum.js

--- src/044-pum.js
+++ src/044-pum.js
@@ -13,9 +13,20 @@
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
+// Aqui é o ponto de entrada, determina a quantidade de linhas que precisamos imprimir no console
 const num = 7;
 
+// Esse for cria as seguintes operações:
+// - Cria uma variável "i" com o valor inicial 1
+// - Repetiremos esse código dentro do escopo do for enquanto essa variável for menor que a quantidade de linhas vezes 4
+// - A cada repetição, o for irá acrescentar 4 ao "i"
+// Nota: por que "num" vezes 4? 
+// 4 É a quantidade que cada linha vai distanciar da anterior: 1, 5, 9, 13
+// Isso faz com que, para repetirmos o for pela quantidade de vezes especificada na variável num,
+// Será necessário multiplicar o valor de num * 4, pois a cada linha, i aumenta 4
 for (let i = 1; i <= num * 4; i += 4) {
+  // Aqui dentro, simplesmente pegamos o "i", imprimimos ele, ele mais 1, e ele mais 2, e a palavra "PUM" no final 
+  // (até agora não se o que significa, mas vamos seguir o exercício ehauhueah)
   console.log(`${i} ${i + 1} ${i + 2} PUM`);
 }
 
