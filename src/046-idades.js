9677e359f4b528545faab3addc107511af307b8e
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-28 01:30:10 -0300

    Update 046-idades.js

--- src/046-idades.js
+++ src/046-idades.js
@@ -1,6 +1,10 @@
+// EXERCÍCIO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
 // URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/1154
 
-// Faça um algoritmo para ler um número indeterminado de dados, contendo cada um, a idade de um indivíduo. O último dado, que não entrará nos cálculos, contém o valor de idade negativa. Calcular e imprimir a idade média deste grupo de indivíduos.
+// Faça um algoritmo para ler um número indeterminado de dados, contendo cada um, a idade de um indivíduo. 
+// O último dado, que não entrará nos cálculos, contém o valor de idade negativa. 
+// Calcular e imprimir a idade média deste grupo de indivíduos.
 
 // Entrada
 // A entrada contém um número indeterminado de inteiros. A entrada será encerrada quando um valor negativo for lido.
@@ -9,6 +13,9 @@
 // A saída contém um valor correspondente à média de idade dos indivíduos.
 
 // A média deve ser impressa com dois dígitos após o ponto decimal.
+
+
+// CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
 // Saída esperada para os valores [34, 56, 44, 23, -2] como entradas do usuários:
 // '39.25'
