c78e8c18b12d11bc15d92bd2b86148f9167c630d
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-29 00:42:31 -0300

    Update 047-conta.js

--- src/047-conta.js
+++ src/047-conta.js
@@ -1,8 +1,4 @@
-const caseLength = Number(lines.shift());
+const num = Number(lines.shift());
 
-for(let caseIndex = 0; caseIndex < caseLength; caseIndex++){
-  const num = Number(lines.shift());
-  
-  if(num % 2 === 0) console.log("0");
-  else console.log("1");
-}
+if (num % 2 === 0) console.log("0");
+else console.log("1");
