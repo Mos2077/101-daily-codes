20e26a277beb0c25e8e7b0f4b049723e260d9cdc
Laks Castro <lakhsmycastro336@gmail.com>
2020-03-31 17:05:08 -0300

    Update 049-troca-em-vetor-i.js

--- src/049-troca-em-vetor-i.js
+++ src/049-troca-em-vetor-i.js
@@ -1,5 +1,11 @@
-const input = require('fs').readFileSync('/dev/stdin', 'utf8');
-const lines = input.split('\n');
+// Faça um programa que leia um vetor N[20]. Troque a seguir, o primeiro elemento com o último, o segundo elemento com o penúltimo, etc., até trocar o 10º com o 11º. Mostre o vetor modificado.
+
+// Entrada
+// A entrada contém 20 valores inteiros, positivos ou negativos.
+
+// Saída
+// Para cada posição do vetor N, escreva "N[i] = Y", onde i é a posição do vetor e Y é o valor armazenado naquela posição.
+
 
 let arr = [];
 
