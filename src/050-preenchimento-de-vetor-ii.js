e49a5a3c38643f428505dab52ce88dfa24b30dbf
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-01 17:14:05 -0300

    Update 050-preenchimento-de-vetor-ii.js

--- src/050-preenchimento-de-vetor-ii.js
+++ src/050-preenchimento-de-vetor-ii.js
@@ -8,10 +8,7 @@
 // Saída
 // Para cada posição do vetor, escreva "N[i] = x", onde i é a posição do vetor e x é o valor armazenado naquela posição.
 
-const input = require('fs').readFileSync('/dev/stdin', 'utf8');
-const lines = input.split('\n');
-
-const qtdLines = Number(lines.shift());
+const qtdLines = 4;
 
 for (let a = 0, b = 0; a < 1000; a++, b === qtdLines - 1 ? (b = 0) : b++) {
   console.log("N[" + a + "] = " + b);
