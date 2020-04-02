4e02a91f41c09cd4b19b20c977adea7a6413de35
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-02 01:48:31 -0300

    Update 051-preenchimento-de-vetor-i.js

--- src/051-preenchimento-de-vetor-i.js
+++ src/051-preenchimento-de-vetor-i.js
@@ -1,1 +1,10 @@
 
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+const n = Number(lines.shift());
+
+for (let a = 0, b = n; a < 10; a++, b *= 2) {
+  console.log("N[" + a + "] = " + b);
+}
