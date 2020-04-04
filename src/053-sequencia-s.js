343106ae028531a746d05dee508a8e8f9210974d
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-04 15:25:39 -0300

    Update 053-sequencia-s.js

--- src/053-sequencia-s.js
+++ src/053-sequencia-s.js
@@ -1,4 +1,4 @@
-
+// EXERCÍCIO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
 // Escreva um algoritmo para calcular e escrever o valor de S, sendo S dado pela fórmula:
 // S = 1 + 1/2 + 1/3 + … + 1/100
@@ -9,6 +9,9 @@
 // Saída
 // A saída contém um valor correspondente ao valor de S.
 // O valor deve ser impresso com dois dígitos após o ponto decimal.
+
+
+// CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
 let s = 1;
 
