2c65d3209855f33c4ffcef12367ba302239d829e
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-04 15:27:26 -0300

    Update 053-sequencia-s.js

--- src/053-sequencia-s.js
+++ src/053-sequencia-s.js
@@ -1,5 +1,7 @@
 // EXERCÍCIO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
+// URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/1155
+
 // Escreva um algoritmo para calcular e escrever o valor de S, sendo S dado pela fórmula:
 // S = 1 + 1/2 + 1/3 + … + 1/100
 
