16ac925b50be7e982932a191d9fe00fa1237303c
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-05 14:47:39 -0300

    Update 054-notas-da-prova.js

--- src/054-notas-da-prova.js
+++ src/054-notas-da-prova.js
@@ -5,7 +5,10 @@
 // Mesmo falando bem inglês, Rosy ficou um pouco apreensiva com a responsabilidade, 
 // mas resolveu aceitar a proposta e encará-la como um bom desafio.
 
-// Tudo ocorreu bem para Rosy até o dia da prova. Acostumada a dar notas de 0 (zero) a 100 (cem), ela fez o mesmo na primeira prova dos alunos da Inglaterra. No entanto, os alunos acharam estranho, pois na Inglaterra o sistema de notas é diferente: as notas devem ser dadas como conceitos de A a E. O conceito A é o mais alto, enquanto o conceito E é o mais baixo.
+// Tudo ocorreu bem para Rosy até o dia da prova. Acostumada a dar notas de 0 (zero) a 100 (cem), 
+// ela fez o mesmo na primeira prova dos alunos da Inglaterra. No entanto, os alunos acharam estranho,
+// pois na Inglaterra o sistema de notas é diferente: as notas devem ser dadas como conceitos de A a E. 
+// O conceito A é o mais alto, enquanto o conceito E é o mais baixo.
 
 // Conversando com outros professores, ela recebeu a sugestão de utilizar a seguinte tabela, 
 // relacionando as notas numéricas com as notas de conceitos:
