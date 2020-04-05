c61fd0da07d00623e2373b58c666b23a83257da3
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-05 14:46:57 -0300

    Update 054-notas-da-prova.js

--- src/054-notas-da-prova.js
+++ src/054-notas-da-prova.js
@@ -7,11 +7,14 @@
 
 // Tudo ocorreu bem para Rosy até o dia da prova. Acostumada a dar notas de 0 (zero) a 100 (cem), ela fez o mesmo na primeira prova dos alunos da Inglaterra. No entanto, os alunos acharam estranho, pois na Inglaterra o sistema de notas é diferente: as notas devem ser dadas como conceitos de A a E. O conceito A é o mais alto, enquanto o conceito E é o mais baixo.
 
-// Conversando com outros professores, ela recebeu a sugestão de utilizar a seguinte tabela, relacionando as notas numéricas com as notas de conceitos:
+// Conversando com outros professores, ela recebeu a sugestão de utilizar a seguinte tabela, 
+// relacionando as notas numéricas com as notas de conceitos:
 
 
 
-// O problema é que Rosy já deu as notas no sistema numérico, e terá que converter as notas para o sistema de letras. Porém, Rosy precisa preparar as próximas aulas (para manter a qualidade que a tornou reconhecida), e não tem tempo suficiente para fazer a conversão das notas manualmente.
+// O problema é que Rosy já deu as notas no sistema numérico, e terá que converter as notas para o sistema de letras. 
+// Porém, Rosy precisa preparar as próximas aulas (para manter a qualidade que a tornou reconhecida), 
+// e não tem tempo suficiente para fazer a conversão das notas manualmente.
 
 // Você deve escrever um programa que recebe uma nota no sistema numérico e determina o conceito correspondente.
 
