68d15e704a740eb0708a152fbc6ab0c1ca2577e7
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-06 16:41:10 -0300

    Update 055-sequencia-logica-2.js

--- src/055-sequencia-logica-2.js
+++ src/055-sequencia-logica-2.js
@@ -13,19 +13,40 @@
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
+// Variável de entrada, que vai armazenar de quanto em quanto que devemos quebrar a linha
 const interval = 5;
+
+// Variável de entrada, que vai armazenar o valor máximo, onde vamos para o laço de repetição
 const max = 30;
 
+// Criamos o laço de repetição da seguinte forma:
+// Iniciamos a variável "a" com o valor 1
+// Vamos continuar repetindo esse laço enquanto a variável "a" for menor que o valor máximo
+// A cada repetição vamos incrementar ao valor de "a" o valor da variável intervalo
 for (let a = 1; a <= max; a += interval) {
+  
+  // Agora, pensando isoladamente para cada repetição (ou seja, para cada linha):
+  
+  // Iniciamos uma variável que vai conter o valor inicial da linha
   const initial = a;
+  // E o valor final da linha
   const final = a + interval;
-
+  
+  // A String que vamos imprimir no console, iniciando vazia
   let currentString = "";
 
+  // Segundo laço de repetição que vai:
+  // Instanciar uma nova variável "a", difente da instanciada acima, com o valor inicial da variável "initial"
+  // Vamos percorrer esse laço enquanto essa variável for menor que a variável "final"
+  // A cada repetição, vamos incrementar 1 à essa variável
   for (let a = initial; a < final; a++) {
+    // Caso seja o último número da linha, então a String vai receber apenas o número
     if (a === final - 1) currentString += a;
+    
+    // Se não, vai receber o número mais um espaço vazio na frente
     else currentString += a + " ";
   }
+  // Ao final, mostramos a String no console
   console.log(currentString);
 }
 
