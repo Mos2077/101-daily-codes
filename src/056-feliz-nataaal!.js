6a61b55095399b2f7816350930299228f6e412ac
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-07 02:05:54 -0300

    Update 056-feliz-nataaal!.js

--- src/056-feliz-nataaal!.js
+++ src/056-feliz-nataaal!.js
@@ -1,3 +1,5 @@
+// EXERCÍCIO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
 // URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/2483
 
 // Você fica tão feliz no natal que tem vontade de gritar para todo mundo: "Feliz natal!!". Pra colocar toda essa felicidade pra fora, você montou um programa que, colocado um índice I de felicidade, seu grito de natal é mais animado.
@@ -7,3 +9,6 @@
 
 // Saída
 // A saída é composta pela frase "Feliz natal!", sendo repetidas I vezes a última letra a da frase. Uma quebra de linha é necessária após a impressão da frase.
+
+
+// CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
