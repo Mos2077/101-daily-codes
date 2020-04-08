2b3296c242446e448cdffedca2b06b6e81ac374d
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-08 14:40:18 -0300

    Update 057-numero-perfeito.js

--- src/057-numero-perfeito.js
+++ src/057-numero-perfeito.js
@@ -21,21 +21,15 @@
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
-const input = require('fs').readFileSync('/dev/stdin', 'utf8');
-const lines = input.split('\n');
+const num = 28;
 
-const casesLength = Number(lines.shift());
+let sum = 0;
 
-for (let caseIndex = 0; caseIndex < casesLength; caseIndex++) {
-  const num = Number(lines.shift());
-
-  let sum = 0;
-
-  for (let a = 1; a < num; a++) {
-    if (num % a === 0) sum += a;
-  }
-
-  const message = num + (sum === num ? " eh perfeito" : " nao eh perfeito");
-
-  console.log(message);
+for (let a = 1; a < num; a++) {
+  if (num % a === 0) sum += a;
 }
+
+const message = num + (sum === num ? " eh perfeito" : " nao eh perfeito");
+
+console.log(message);
+
