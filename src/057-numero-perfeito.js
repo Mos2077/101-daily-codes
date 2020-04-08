5c3ae13322b299472c72a85331addccae312eab3
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-08 14:36:29 -0300

    Update 057-numero-perfeito.js

--- src/057-numero-perfeito.js
+++ src/057-numero-perfeito.js
@@ -1,3 +1,4 @@
+// URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/1164
 
 // Na matemática, um número perfeito é um número inteiro para o qual a soma de todos os seus divisores positivos próprios (excluindo ele mesmo) é igual ao próprio número. Por exemplo o número 6 é perfeito, pois 1+2+3 é igual a 6. Sua tarefa é escrever um programa que imprima se um determinado número é perfeito ou não.
 
