77b14fb79d29596f0d37a48d17d651573439843c
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-09 15:44:31 -0300

    Update 058-insect!.js

--- src/058-insect!.js
+++ src/058-insect!.js
@@ -13,3 +13,19 @@
 
 // Saída
 // Para cada valor lido, imprima o texto correspondente.
+
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+const casesLength = Number(lines.shift());
+
+for (let caseIndex = 0; caseIndex < casesLength; caseIndex++) {
+  const level = Number(lines.shift());
+
+  if (level <= 8000) {
+    console.log("Inseto!");
+    continue;
+  }
+  console.log("Mais de 8000!");
+}
