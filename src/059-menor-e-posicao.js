d8f0f9367530f6fddd5f586536f84fc39ebc34bf
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-10 10:09:08 -0300

    Update 059-menor-e-posicao.js

--- src/059-menor-e-posicao.js
+++ src/059-menor-e-posicao.js
@@ -11,20 +11,34 @@
 // A primeira linha apresenta a mensagem “Menor valor:” seguida de um espaço e do menor valor lido na entrada. A segunda linha apresenta a mensagem “Posicao:” seguido de um espaço e da posição do vetor na qual se encontra o menor valor lido, lembrando que o vetor inicia na posição zero.
 
 
+// Tamanho do array
 const n = 5;
+
+// Array de números a ser extraído o menor
 const arrayN = [2, 6, 2, 5, -6];
 
+// Variável que armazena o menor, começando pelo primeiro elemento
 let lowest = arrayN[0];
+
+// Variável que armazena o index do menor número, começando pelo primeiro
 let lowestIndex = 0;
 
+// Array que vai percorrer os restantes elementos do array
 for (let index = 1; index < n; index++) {
+  // Pegamos o atual elemento a partir do index do "for"
   const currentNumber = arrayN[index];
+  
+  // Caso esse número seja ainda menor que o menor número que temos registrado...
   if (currentNumber < lowest) {
+    // ... então registramos esse número como o menor já registrado
     lowest = currentNumber;
+    
+    // O mesmo para o index
     lowestIndex = index;
   }
 }
 
+// Mostramos no console o valor e o index do menor valor
 console.log("Menor valor: " + lowest);
 console.log("Posicao: " + lowestIndex);
 
