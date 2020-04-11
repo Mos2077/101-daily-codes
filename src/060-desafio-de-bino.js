ccc39c0a7e3ee53a9ecfdd3c8214cfa7c1bca989
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-11 10:57:19 -0300

    Update 060-desafio-de-bino.js

--- src/060-desafio-de-bino.js
+++ src/060-desafio-de-bino.js
@@ -10,3 +10,38 @@
 
 // Saída
 // Imprima a quantidade de números múltiplos de 2, 3, 4 e 5 presentes na lista. Observe a formatação da saída nos exemplos, pois ela deve ser seguida rigorosamente.
+
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+let multiples = {
+  "2": {
+    count: 0
+  },
+  "3": {
+    count: 0
+  },
+  "4": {
+    count: 0
+  },
+  "5": {
+    count: 0
+  }
+};
+
+const getKeys = func => Object.keys(multiples).forEach(func);
+
+const addMultiple = a =>
+  getKeys(key => {
+    a % Number(key) === 0 && multiples[key].count++;
+  });
+
+const n = Number(lines.shift());
+
+const arr = lines
+  .shift()
+  .split(" ")
+  .forEach(item => addMultiple(Number(item)));
+
+getKeys(key => console.log(multiples[key].count + " Multiplo(s) de " + key));
