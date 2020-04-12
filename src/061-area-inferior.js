bfd7ccaf1bcd30c8eae5a5ee068c5a70347ed922
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-12 17:00:47 -0300

    Update 061-area-inferior.js

--- src/061-area-inferior.js
+++ src/061-area-inferior.js
@@ -1,1 +1,29 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
 
+const op = lines.shift() === "S" ? value => value : value => value / 144;
+
+const [x, y] = [12, 12];
+
+const values = [];
+
+let initOn = 5;
+let endOn = 6;
+
+let sum = 0;
+
+for (let a = 0; a < x; a++) {
+  for (let b = 0; b < y; b++) {
+    const value = Number(lines.shift());
+
+    if (a >= 7 && b >= initOn && b <= endOn) {
+      sum += value;
+    }
+  }
+  if (a >= 7) {
+    initOn--;
+    endOn++;
+  }
+}
+
+console.log(op(sum).toFixed(1));
