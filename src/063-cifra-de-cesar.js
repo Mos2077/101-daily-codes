c1e328bc1053ab173d37c0831f2659861cc0d6c1
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-14 18:31:22 -0300

    Update 063-cifra-de-cesar.js

--- src/063-cifra-de-cesar.js
+++ src/063-cifra-de-cesar.js
@@ -11,26 +11,20 @@
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
-const input = require('fs').readFileSync('/dev/stdin', 'utf8');
-const lines = input.split('\n');
+const str = "LIPPSASVPH";
+const key = 4;
 
-const cases = Number(lines.shift());
+let newStr = "";
 
-for (let caseInd = 0; caseInd < cases; caseInd++) {
-  const str = lines.shift();
-  const key = Number(lines.shift());
+for (let i = 0; i < str.length; i++) {
+  const code = str.charCodeAt(i);
 
-  let newStr = "";
+  let newCode = code - key;
 
-  for (let i = 0; i < str.length; i++) {
-    const code = str.charCodeAt(i);
+  if (newCode < 65) newCode = 91 - (65 - newCode);
 
-    let newCode = code - key;
-
-    if (newCode < 65) newCode = 91 - (65 - newCode);
-
-    newStr += String.fromCharCode(newCode);
-  }
-
-  console.log(newStr);
+  newStr += String.fromCharCode(newCode);
 }
+
+console.log(newStr);
+
