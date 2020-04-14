63ee3a7270c75ea5b364a58df48a9ff7226033a3
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-14 18:41:15 -0300

    Update 063-cifra-de-cesar.js

--- src/063-cifra-de-cesar.js
+++ src/063-cifra-de-cesar.js
@@ -8,10 +8,18 @@
 // Nós podemos, é claro, tentar trocar as letras com quaisquer número de posições.
 
 // Entrada
-// A entrada contém vários casos de teste. A primeira linha de entrada contém um inteiro N que indica a quantidade de casos de teste. Cada caso de teste é composto por duas linhas. A primeira linha contém uma string com até 50 caracteres maiúsculos ('A'-'Z'), que é a sentença após ela ter sido codificada através desta Cifra de César modificada. A segunda linha contém um número que varia de 0 a 25 e que representa quantas posições cada letra foi deslocada para a direita.
+// A entrada contém vários casos de teste.
+// A primeira linha de entrada contém um inteiro N que indica a quantidade de casos de teste. 
+// Cada caso de teste é composto por duas linhas.
+// A primeira linha contém uma string com até 50 caracteres maiúsculos ('A'-'Z'), 
+// que é a sentença após ela ter sido codificada através desta Cifra de César modificada.
+// A segunda linha contém um número que varia de 0 a 25 
+// e que representa quantas posições cada letra foi deslocada para a direita.
 
 // Saída
-// Para cada caso de teste de entrada, imprima uma linha de saída com o texto decodificado (transformado novamente para o texto original) conforme as regras acima e o exemplo abaixo.
+// Para cada caso de teste de entrada,
+// imprima uma linha de saída com o texto decodificado (transformado novamente para o texto original) 
+// conforme as regras acima e o exemplo abaixo.
 
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
