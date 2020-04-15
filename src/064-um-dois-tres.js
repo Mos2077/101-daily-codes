582c4cb77e06d848b3298d215327c76ca470b6da
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-15 17:00:56 -0300

    Update 064-um-dois-tres.js

--- src/064-um-dois-tres.js
+++ src/064-um-dois-tres.js
@@ -24,13 +24,23 @@
 // Ponto de entrada, são as letras que a criança escreveu
 const str = "twn";
 
+// Temos os casos 1, 2 e 3, 3 é a única palavra em ingles dentre as três, que tem mais de 3 letras, então:
+// Se o tamanho da String for maior que três...
 if (str.length > 3) {
+  // Ele quer dizer 3
   console.log("3");
 } else {
+  // Se não for, então vamos procurar por:
+  // Vamos ver se ele quer falar 1 (one):
+  // A regex /.ne|o.e|on./ quer dizer: a string tem esse formato: Xne ou oXe ou onX?
+  // Onde X representa qualquer outra letra, afinal, ele pode errar uma letra
+  // Se tiver esse formato, então ele quer dizer 1
   const isOne = str.match(/.ne|o.e|on./);
   if (isOne) {
+    // Imprimimos
     console.log("1");
   } else {
+    // Fazemos a mesma coisa só que com 2 em inglês: a palavra "two"
     const isTwo = str.match(/.wo|t.o|tw./);
     if (isTwo) console.log("2");
   }
