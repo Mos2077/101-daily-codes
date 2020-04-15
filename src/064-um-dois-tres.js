1a4829712083578a6c8ae7d6c70ae93b9399fecc
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-15 16:49:29 -0300

    Update 064-um-dois-tres.js

--- src/064-um-dois-tres.js
+++ src/064-um-dois-tres.js
@@ -21,25 +21,19 @@
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
-const input = require('fs').readFileSync('/dev/stdin', 'utf8');
-const lines = input.split('\n');
+const str = "twn";
 
-const cases = Number(lines.shift());
-
-for (let caseIndex = 0; caseIndex < cases; caseIndex++) {
-  const str = lines.shift();
-
-  if (str.length > 3) {
-    console.log("3");
-    continue;
-  }
-
+if (str.length > 3) {
+  console.log("3");
+} else {
   const isOne = str.match(/.ne|o.e|on./);
   if (isOne) {
     console.log("1");
-    continue;
+  } else {
+    const isTwo = str.match(/.wo|t.o|tw./);
+    if (isTwo) console.log("2");
   }
-
-  const isTwo = str.match(/.wo|t.o|tw./);
-  if (isTwo) console.log("2");
 }
+
+
+
