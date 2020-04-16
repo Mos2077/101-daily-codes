9dcb2c21ccfce49d769a157684ffb932d9502484
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-16 02:34:35 -0300

    Update 065-numero-primo.js

--- src/065-numero-primo.js
+++ src/065-numero-primo.js
@@ -1,3 +1,4 @@
+// EXERCÍCIO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
 // Na matemática, um Número Primo é aquele que pode ser dividido somente por 1 (um) e por ele mesmo. Por exemplo, o número 7 é primo, pois pode ser dividido apenas pelo número 1 e pelo número 7.
 
