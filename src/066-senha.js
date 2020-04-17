873d11654c13b6b4c749b382c61844f32f108251
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-17 17:50:10 -0300

    Update 066-senha.js

--- src/066-senha.js
+++ src/066-senha.js
@@ -1,1 +1,15 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
 
+let getout = false;
+
+while (!getout) {
+  const a = Number(lines.shift());
+
+  if (!a) {
+    getout = true;
+    break;
+  }
+
+  console.log(a - 1);
+}
