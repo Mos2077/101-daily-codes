54cde6c1bbd7d5cec967cbf551b81cf8671e7a07
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-18 17:00:44 -0300

    Update 067-andando-no-tempo.js

--- src/067-andando-no-tempo.js
+++ src/067-andando-no-tempo.js
@@ -7,3 +7,18 @@
 
 // Saída
 // Seu programa deve imprimir uma linha contendo o caracter “S” se é poss ível viajar e voltar para o presente, ou “N” caso contrário.
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+const credits = lines.shift();
+
+const [a, b, c] = credits.split(" ").map(credit => Number(credit));
+
+let isPossible = a + b === c || a + c === b || b + c === a;
+
+if (!isPossible) isPossible = a - b === c || a - c === b || b - c === a;
+
+if (!isPossible) isPossible = a === b || b === c || c === a;
+
+console.log(isPossible ? "S" : "N");
