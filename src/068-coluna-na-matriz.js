fee7e44c70935ae4ad45d411d4a6f8ef2e77cf15
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-19 21:23:28 -0300

    Update 068-coluna-na-matriz.js

--- src/068-coluna-na-matriz.js
+++ src/068-coluna-na-matriz.js
@@ -8,3 +8,24 @@ A primeira linha de entrada contem um número C (0 ≤ C ≤ 11) indicando a col
 
 Saída
 Imprima o resultado solicitado (a soma ou média), com 1 casa após o ponto decimal.
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+
+
+const lines = input.split('\n');
+
+const column = Number(lines.shift());
+
+const op = lines.shift();
+
+let sum = 0;
+
+for (let x = 0; x < 12; x++) {
+  for (let y = 0; y < 12; y++) {
+    const num = Number(lines.shift());
+
+    if (y === column) sum += num;
+  }
+}
+
+console.log((op === "S" ? sum : sum / 12).toFixed(1));
