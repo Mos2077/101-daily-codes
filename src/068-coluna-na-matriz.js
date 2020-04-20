59b5d6e043cc459b4c9694f1eb7aed4db9c1ccbf
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-19 21:34:22 -0300

    Update 068-coluna-na-matriz.js

--- src/068-coluna-na-matriz.js
+++ src/068-coluna-na-matriz.js
@@ -20,18 +20,37 @@
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
-const column = Number(lines.shift());
+// Coluna da matriz 12x12 que será considerada
+const column = 2;
 
-const op = lines.shift();
+// Operação que iremos fazer ao considerar todos os elementos da X coluna
+// M para mostrar a média
+// S para mostrar a soma
+const op = "M";
 
+// Para ambos vamos precisar da soma dos termos,
+// Então criamos a variável "sum" começando pelo 0
 let sum = 0;
 
+// Criamos um for que repete 12 vezes
 for (let x = 0; x < 12; x++) {
+  // E dentro desse for, criamos outro for que repete também 12 vezes
+  // 12 x 12, dá uma matriz 12 por 12
+  // y é a variável que armazena a coluna da atual linha
   for (let y = 0; y < 12; y++) {
-    const num = Number(lines.shift());
-
+    // Lemos elemento
+    // Vamos considerar todos os elementos como 5, 
+    // portanto, é uma matriz 12 por 12 com todos os elementos igual a 5
+    const num = 5;
+    
+    // Caso a coluna atual do laço seja igual à coluna especificada no início do programa,
+    // Então, adicionamos o valor do elemento atual à variável sum, 
+    // pois ele é um elemento a ser incrementado
     if (y === column) sum += num;
   }
 }
 
+// Por fim, mostramos no console a soma, caso a OPeração escolhida for igual a "S"
+// Ou mostramos a média, que é a mesma soma, só que dividida por 2
+// .toFixed(1) para obrigar os números a terem uma casa decimal
 console.log((op === "S" ? sum : sum / 12).toFixed(1));
