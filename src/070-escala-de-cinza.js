dac7af52bec5063d539d408de5409f2a91ff01f6
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-21 16:04:26 -0300

    Update 070-escala-de-cinza.js

--- src/070-escala-de-cinza.js
+++ src/070-escala-de-cinza.js
@@ -32,9 +32,6 @@
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
-const input = require('fs').readFileSync('/dev/stdin', 'utf8');
-const lines = input.split('\n');
-
 const op = {
   eye: (r, g, b) => r * 0.3 + g * 0.59 + b * 0.11,
   mean: (r, g, b) => (r + g + b) / 3,
@@ -44,11 +41,9 @@ const op = {
 
 const cases = Number(lines.shift());
 
-for (let caseIndex = 1; caseIndex <= cases; caseIndex++) {
   const operation = lines.shift();
   const color = lines.shift();
 
   const [r, g, b] = color.split(" ").map(cor => Number(cor));
 
   console.log("Caso #" + caseIndex + ": " + parseInt(op[operation](r, g, b)));
-}
