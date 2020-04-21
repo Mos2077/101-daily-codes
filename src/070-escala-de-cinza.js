886eef200f51907fe7da7f4285f8fd0aa369d0da
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-21 15:55:02 -0300

    Update 070-escala-de-cinza.js

--- src/070-escala-de-cinza.js
+++ src/070-escala-de-cinza.js
@@ -13,3 +13,26 @@ A entrada em T (1 ≤ T ≤ 100) casos de teste, onde o valor de T é dado na pr
 
 Saída
 Para cada caso de testes dever ser impressa a seguinte mensagem "Caso #t: P", onde P é o nível de cinza do pixel da imagem em tons de cinza após a conversão do pixel da imagem colorida. Esta mensagem deve ser seguida de uma quebra de linha.
+
+
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+const op = {
+  eye: (r, g, b) => r * 0.3 + g * 0.59 + b * 0.11,
+  mean: (r, g, b) => (r + g + b) / 3,
+  max: (...color) => Math.max(...color),
+  min: (...color) => Math.min(...color)
+};
+
+const cases = Number(lines.shift());
+
+for (let caseIndex = 1; caseIndex <= cases; caseIndex++) {
+  const operation = lines.shift();
+  const color = lines.shift();
+
+  const [r, g, b] = color.split(" ").map(cor => Number(cor));
+
+  console.log("Caso #" + caseIndex + ": " + parseInt(op[operation](r, g, b)));
+}
