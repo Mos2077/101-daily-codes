55f2ee03264aea94ebf3da25c0d770bd41dbbe7f
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-22 18:23:09 -0300

    Update 071-blobs.js

--- src/071-blobs.js
+++ src/071-blobs.js
@@ -8,6 +8,8 @@
 // Saída
 // Para cada caso de teste, imprima uma linha contendo o número de dias que Blobs irá demorar para comer todo seu suprimento de comida, seguido da palavra "dias".
 
+// CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
 let kg = Number(lines.shift());
 
 let days = 0;
