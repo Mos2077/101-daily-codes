399eb9db90647770aa62667bc5a89c866b3bab9b
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-22 18:01:45 -0300

    Update 071-blobs.js

--- src/071-blobs.js
+++ src/071-blobs.js
@@ -8,3 +8,22 @@ A primeira linha de entrada contem um único inteiro N (1 ≤ N ≤ 1000), indic
 
 Saída
 Para cada caso de teste, imprima uma linha contendo o número de dias que Blobs irá demorar para comer todo seu suprimento de comida, seguido da palavra "dias".
+
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+const cases = Number(lines.shift());
+
+for (let cas = 0; cas < cases; cas++) {
+  let kg = Number(lines.shift());
+
+  let days = 0;
+
+  for (; kg > 1; ) {
+    days++;
+    kg /= 2;
+  }
+
+  console.log(days + " dias");
+}
