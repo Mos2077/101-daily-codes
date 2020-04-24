de390ac5bedf060ef749965972ab265d96fb2162
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-23 22:19:00 -0300

    Update 072-array-hash.js

--- src/072-array-hash.js
+++ src/072-array-hash.js
@@ -22,3 +22,28 @@ A entrada contém vários casos de teste. A primeira linha de entrada contém um
 
 Saída
 Para cada caso de teste imprima o valor de hash que é calculado conforme o exemplo apresentado acima.
+
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+const getIndex = a => a.charCodeAt() - 65;
+
+const cases = Number(lines.shift());
+
+for (let cas = 0; cas < cases; cas++) {
+  const l = Number(lines.shift());
+  let sum = 0;
+
+  for (let k = 0; k < l; k++) {
+    const str = lines.shift();
+
+    str.split("").forEach((item, i) => {
+      const value = getIndex(item) + i + k;
+
+      sum += value;
+    });
+  }
+
+  console.log(sum);
+}
