7093b5584ca995c4a9add868be08a089c4497b78
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-24 17:15:02 -0300

    Update 073-jogo-do-tijolo.js

--- src/073-jogo-do-tijolo.js
+++ src/073-jogo-do-tijolo.js
@@ -13,6 +13,8 @@ Saída
 Para cada caso de teste, imprima uma linha no formato “Case x: a”\(sem as aspas), onde x é o número do caso de teste e a é a idade do capitão.
 
 
+CÓDIGO
+
 const input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = input.split('\n');
 
