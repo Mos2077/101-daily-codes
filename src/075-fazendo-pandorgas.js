5da6b90d7ca29da2624c060a7576836d4f430c07
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-26 15:55:42 -0300

    Update 075-fazendo-pandorgas.js

--- src/075-fazendo-pandorgas.js
+++ src/075-fazendo-pandorgas.js
@@ -13,11 +13,13 @@
 
 // Entrada
 // A entrada contém vários casos de teste. A primeira linha de entrada contém um inteiro N 
-//que indica a quantidade de pipas que serão construídas. 
-// Cada uma das N linhas a seguir contém dois valores inteiros x (10 ≤ x ≤ 100) e y (10 ≤ y ≤ 100) que indicam o tamanho dos dois bambus utilizados para construir a pipa.
+// que indica a quantidade de pipas que serão construídas. 
+// Cada uma das N linhas a seguir contém dois valores inteiros x (10 ≤ x ≤ 100) e y (10 ≤ y ≤ 100) 
+// que indicam o tamanho dos dois bambus utilizados para construir a pipa.
 
 // Saída
-// Para cada caso de teste de entrada, imprima um valor inteiro (desconsiderando a parte decimal) correspondente a àrea da pipa criada, em cm2, seguido de um espaço e do texto "cm2", sem as aspas.
+// Para cada caso de teste de entrada, imprima um valor inteiro (desconsiderando a parte decimal)
+// correspondente a àrea da pipa criada, em cm2, seguido de um espaço e do texto "cm2", sem as aspas.
 
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
