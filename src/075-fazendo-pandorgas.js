f59f90fbb04d8389c147dbac33539f8cac79c85d
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-26 15:54:01 -0300

    Update 075-fazendo-pandorgas.js

--- src/075-fazendo-pandorgas.js
+++ src/075-fazendo-pandorgas.js
@@ -17,6 +17,7 @@
 // Saída
 // Para cada caso de teste de entrada, imprima um valor inteiro (desconsiderando a parte decimal) correspondente a àrea da pipa criada, em cm2, seguido de um espaço e do texto "cm2", sem as aspas.
 
+
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
 
