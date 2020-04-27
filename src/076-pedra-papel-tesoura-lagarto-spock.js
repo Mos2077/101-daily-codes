aab6911796bd5bfd504fe06822a27f1870736b7e
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-27 18:12:20 -0300

    Update 076-pedra-papel-tesoura-lagarto-spock.js

--- src/076-pedra-papel-tesoura-lagarto-spock.js
+++ src/076-pedra-papel-tesoura-lagarto-spock.js
@@ -28,11 +28,11 @@
 // Saída
 // Para cada caso de teste, imprima quem venceu, ou se houve empate.
 
+CÓDIGO
 
 const input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = input.split('\n');
 
-CÓDIGO
 
 const enumGame = [
   ["tesoura", "papel"],
