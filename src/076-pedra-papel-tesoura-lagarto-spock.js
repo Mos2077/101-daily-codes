fd6105dc580c889f8d5c4afd983981ec55e5d334
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-27 18:10:37 -0300

    Update 076-pedra-papel-tesoura-lagarto-spock.js

--- src/076-pedra-papel-tesoura-lagarto-spock.js
+++ src/076-pedra-papel-tesoura-lagarto-spock.js
@@ -16,8 +16,8 @@
 // Pedra quebra tesoura
 // Um dia, dois amigos, Rajesh e Sheldon, decidiram apostar quem pagaria um almoço para o outro, com esta brincadeira. Sua missão será fazer um algoritmo que, baseado no que eles escolherem, informe quem irá ganhar ou se dará empate.
 
-Entrada
-Haverá diversos casos de teste. O primeiro número a ser lido será um inteiro C, representando a quantidade de casos de teste. Cada caso de teste tem duas palavras, representando a escolha de Rajesh e de Sheldon, respectivamente.
+// Entrada
+// Haverá diversos casos de teste. O primeiro número a ser lido será um inteiro C, representando a quantidade de casos de teste. Cada caso de teste tem duas palavras, representando a escolha de Rajesh e de Sheldon, respectivamente.
 
 Saída
 Para cada caso de teste, imprima quem venceu, ou se houve empate.
