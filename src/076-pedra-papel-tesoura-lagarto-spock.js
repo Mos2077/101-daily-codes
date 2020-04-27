2418112bfdbd9b743cbf7f04559187bb0b7d6b92
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-27 18:11:06 -0300

    Update 076-pedra-papel-tesoura-lagarto-spock.js

--- src/076-pedra-papel-tesoura-lagarto-spock.js
+++ src/076-pedra-papel-tesoura-lagarto-spock.js
@@ -2,7 +2,10 @@
 
 // https://www.urionlinejudge.com.br/judge/pt/problems/view/1873
 
-// Pedra-papel-tesoura-lagarto-Spock é uma expansão do clássico método de seleção em jogo de pedra-papel-tesoura. Atua sob o mesmo princípio básico, mas inclui outras duas armas adicionais: o lagarto (formado pela mão igual a uma boca de fantoche) e Spock (formada pela saudação dos vulcanos em Star Trek). Isso reduz as chances de uma rodada terminar em um empate. O jogo foi inventado por Sam Kass e Karen Bryla, como "Rock Paper Scissors Lizard Spock". As regras de vantagem são as seguintes:
+// Pedra-papel-tesoura-lagarto-Spock é uma expansão do clássico método de seleção em jogo de pedra-papel-tesoura.
+// Atua sob o mesmo princípio básico, mas inclui outras duas armas adicionais:
+// o lagarto (formado pela mão igual a uma boca de fantoche) e Spock (formada pela saudação dos vulcanos em Star Trek). 
+// Isso reduz as chances de uma rodada terminar em um empate. O jogo foi inventado por Sam Kass e Karen Bryla, como "Rock Paper Scissors Lizard Spock". As regras de vantagem são as seguintes:
 
 // Tesoura corta papel
 // Papel cobre pedra
