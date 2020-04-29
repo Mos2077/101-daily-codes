94df10f831abc96028ca153927eb5888e71b28fd
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-28 22:00:04 -0300

    Update 077-bob-conduit.js

--- src/077-bob-conduit.js
+++ src/077-bob-conduit.js
@@ -1,6 +1,6 @@
 // EXERCÍCIO
 
-// https://www.urionlinejudge.com.br/judge/pt/problems/view/1589
+// URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/1589
 
 Você tem em mãos dois cabos circulares de energia. O primeiro cabo tem raio R1 e o segundo raio R2. Você precisa comprar um conduite circular (veja a imagem abaixo que ilustra um conduite) de maneira a passar os dois cabos por dentro dele:
 
