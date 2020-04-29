1b3e68d1c0b76dfe456631a648294042b02b27f9
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-28 22:04:26 -0300

    Update 077-bob-conduit.js

--- src/077-bob-conduit.js
+++ src/077-bob-conduit.js
@@ -13,9 +13,9 @@
 
 // Entrada
 // Na primeira linha teremos um inteiro T (T ≤ 10000), indicando o número de casos de teste.
-
 // Na única linha de cada caso teremos dois números inteiros R1 e R2, indicando os respectivos raios. 
 // Os inteiros serão positivos e todas as contas caberão em um inteiro normal de 32 bits.
+
 // Saída
 // Em cada caso, imprima o menor raio possível em uma única linha
 
