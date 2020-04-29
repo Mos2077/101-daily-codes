e52b2d517911c523c007ce072bf673118fffc5c5
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-28 22:01:22 -0300

    Update 077-bob-conduit.js

--- src/077-bob-conduit.js
+++ src/077-bob-conduit.js
@@ -9,11 +9,11 @@
 // Qual o menor raio do conduite que você deve comprar? Em outras palavras, dado dois círculos, qual o raio do menor círculo que possa englobar ambos os dois?
 
 // Entrada
-Na primeira linha teremos um inteiro T (T ≤ 10000), indicando o número de casos de teste.
+// Na primeira linha teremos um inteiro T (T ≤ 10000), indicando o número de casos de teste.
 
-Na única linha de cada caso teremos dois números inteiros R1 e R2, indicando os respectivos raios. Os inteiros serão positivos e todas as contas caberão em um inteiro normal de 32 bits.
-Saída
-Em cada caso, imprima o menor raio possível em uma única linha
+// Na única linha de cada caso teremos dois números inteiros R1 e R2, indicando os respectivos raios. Os inteiros serão positivos e todas as contas caberão em um inteiro normal de 32 bits.
+// Saída
+// Em cada caso, imprima o menor raio possível em uma única linha
 
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
