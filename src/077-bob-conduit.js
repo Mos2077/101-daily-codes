904101bb7f19bc80966136905a409dea1e61d46c
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-28 21:51:48 -0300

    Update 077-bob-conduit.js

--- src/077-bob-conduit.js
+++ src/077-bob-conduit.js
@@ -1,5 +1,19 @@
 // https://www.urionlinejudge.com.br/judge/pt/problems/view/1589
 
+Você tem em mãos dois cabos circulares de energia. O primeiro cabo tem raio R1 e o segundo raio R2. Você precisa comprar um conduite circular (veja a imagem abaixo que ilustra um conduite) de maneira a passar os dois cabos por dentro dele:
+
+
+
+Qual o menor raio do conduite que você deve comprar? Em outras palavras, dado dois círculos, qual o raio do menor círculo que possa englobar ambos os dois?
+
+Entrada
+Na primeira linha teremos um inteiro T (T ≤ 10000), indicando o número de casos de teste.
+
+Na única linha de cada caso teremos dois números inteiros R1 e R2, indicando os respectivos raios. Os inteiros serão positivos e todas as contas caberão em um inteiro normal de 32 bits.
+Saída
+Em cada caso, imprima o menor raio possível em uma única linha
+
+
 const input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = input.split('\n');
 
@@ -15,16 +29,3 @@ for (let cas = 0; cas < cases; cas++) {
 
   console.log(r);
 }
-
-Você tem em mãos dois cabos circulares de energia. O primeiro cabo tem raio R1 e o segundo raio R2. Você precisa comprar um conduite circular (veja a imagem abaixo que ilustra um conduite) de maneira a passar os dois cabos por dentro dele:
-
-
-
-Qual o menor raio do conduite que você deve comprar? Em outras palavras, dado dois círculos, qual o raio do menor círculo que possa englobar ambos os dois?
-
-Entrada
-Na primeira linha teremos um inteiro T (T ≤ 10000), indicando o número de casos de teste.
-
-Na única linha de cada caso teremos dois números inteiros R1 e R2, indicando os respectivos raios. Os inteiros serão positivos e todas as contas caberão em um inteiro normal de 32 bits.
-Saída
-Em cada caso, imprima o menor raio possível em uma única linha
