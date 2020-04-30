4bdba2b51f76bd2457e62866db04b492ed82cb38
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-29 21:33:52 -0300

    Update 078-nossos-dias-nunca-voltarao.js

--- src/078-nossos-dias-nunca-voltarao.js
+++ src/078-nossos-dias-nunca-voltarao.js
@@ -17,7 +17,10 @@
 // A entrada consiste de um único inteiro N (1 ≤ N ≤ 34) numa linha.
 
 // Saída
-// Imprima os N primeiros caracteres da citação de Søren Kierkegaard definida pelas letras que foram sublinhadas no enunciado deste problema. Preste atenção, pois nenhum espaço foi sublinhado — você é que deve adivinhar o número e a localização dos espaços na frase. A única linha da saída deve consistir apenas de letras maiúsculas e espaços e ser finalizada por fim de linha.
+// Imprima os N primeiros caracteres da citação de Søren Kierkegaard definida pelas letras que 
+// foram sublinhadas no enunciado deste problema. Preste atenção,
+// pois nenhum espaço foi sublinhado — você é que deve adivinhar o número e a localização dos espaços na frase. 
+// A única linha da saída deve consistir apenas de letras maiúsculas e espaços e ser finalizada por fim de linha.
 
 // CÓDIGO DO CONSOLE <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
