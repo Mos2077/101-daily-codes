d0db6eb364579c65525bcf1a660a74552697de3e
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-29 21:32:32 -0300

    Update 078-nossos-dias-nunca-voltarao.js

--- src/078-nossos-dias-nunca-voltarao.js
+++ src/078-nossos-dias-nunca-voltarao.js
@@ -7,10 +7,10 @@
 // Esperamos que você tenha gostado desses últimos dias em Essos e em Westeros, que tenha aprendido bastante e que tenha se divertido. Mas não é só em Essos e em Westeros que você deve se divertir. Aqui, em Além da Muralha, programar também é divertido. Continue estudando, continue treinando, e cada vez mais. O importante é o caminho que você vai trilhar daqui para frente. Nosso conselho é que você procure sempre aproveitar ao máximo cada momento, cada oficina, cada escola, cada treino, cada tempo de prática ou estudo em casa. Nossos dias nunca voltarão.
 
 // Entrada
-A entrada consiste de um único inteiro N (1 ≤ N ≤ 34) numa linha.
+// A entrada consiste de um único inteiro N (1 ≤ N ≤ 34) numa linha.
 
-Saída
-Imprima os N primeiros caracteres da citação de Søren Kierkegaard definida pelas letras que foram sublinhadas no enunciado deste problema. Preste atenção, pois nenhum espaço foi sublinhado — você é que deve adivinhar o número e a localização dos espaços na frase. A única linha da saída deve consistir apenas de letras maiúsculas e espaços e ser finalizada por fim de linha.
+// Saída
+// Imprima os N primeiros caracteres da citação de Søren Kierkegaard definida pelas letras que foram sublinhadas no enunciado deste problema. Preste atenção, pois nenhum espaço foi sublinhado — você é que deve adivinhar o número e a localização dos espaços na frase. A única linha da saída deve consistir apenas de letras maiúsculas e espaços e ser finalizada por fim de linha.
 
 // CÓDIGO DO CONSOLE <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
