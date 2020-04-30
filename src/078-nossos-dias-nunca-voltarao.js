01104035b6a5181a78a65928eab22f8e0d653a28
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-29 21:35:54 -0300

    Update 078-nossos-dias-nunca-voltarao.js

--- src/078-nossos-dias-nunca-voltarao.js
+++ src/078-nossos-dias-nunca-voltarao.js
@@ -5,7 +5,8 @@
 // E aí? Curtiu a Escola de Inverno deste ano? Para que esta Escola acontecesse, foram muitos que trabalharam, 
 // seja na elaboração dos problemas, na configuração do Portal, na logística do evento ou na captação dos recursos.
 // Nosso agradecimento especial deste ano vai para o Prof. Ricardo Oliveira, que não somente aceitou vir ministrar as oficinas 
-// como também participou ativamente na organização da Escola. Temos certeza que a experiência e a carreira dele no ICPC como competidor e como coach motivaram e inspiraram todos nós.
+// como também participou ativamente na organização da Escola.
+// Temos certeza que a experiência e a carreira dele no ICPC como competidor e como coach motivaram e inspiraram todos nós.
 
 // Esperamos que você tenha gostado desses últimos dias em Essos e em Westeros, que tenha aprendido bastante e que tenha se divertido.
 // Mas não é só em Essos e em Westeros que você deve se divertir. Aqui, em Além da Muralha, programar também é divertido. 
