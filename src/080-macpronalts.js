83dbcc7856132b9ce43feb41f611ea9809248709
Laks Castro <lakhsmycastro336@gmail.com>
2020-05-01 18:00:17 -0300

    Update 080-macpronalts.js

--- src/080-macpronalts.js
+++ src/080-macpronalts.js
@@ -4,7 +4,9 @@
 
 // O MacPRONALTS está com uma super promoção, exclusivo para os competidores da primeira Seletiva do MaratonaTEC.
 // Só que teve um problema, todos os maratonistas foram tentar comprar ao mesmo tempo, com isso gerou uma fila muito grande. 
-// O pior é que a moça do caixa estava sem calculadora ou um programa para ajudá-la a calcular com maior agilidade, eis que surge você para fazer um programa para ajudar a coitada e aumentar a renda do MacPRONALTS. Segue o cardápio do dia contendo o número do produto e seu respectivo valor.
+// O pior é que a moça do caixa estava sem calculadora ou um programa para ajudá-la a calcular com maior agilidade,
+// eis que surge você para fazer um programa para ajudar a coitada e aumentar a renda do MacPRONALTS.
+// Segue o cardápio do dia contendo o número do produto e seu respectivo valor.
 
 // 1001 | R$ 1.50
 // 1002 | R$ 2.50
