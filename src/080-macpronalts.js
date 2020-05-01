074fec7974eb21114ae0f34011ceafba1b42c989
Laks Castro <lakhsmycastro336@gmail.com>
2020-05-01 17:55:51 -0300

    Update 080-macpronalts.js

--- src/080-macpronalts.js
+++ src/080-macpronalts.js
@@ -6,11 +6,9 @@
 // Só que teve um problema, todos os maratonistas foram tentar comprar ao mesmo tempo, com isso gerou uma fila muito grande. 
 // O pior é que a moça do caixa estava sem calculadora ou um programa para ajudá-la a calcular com maior agilidade, eis que surge você para fazer um programa para ajudar a coitada e aumentar a renda do MacPRONALTS. Segue o cardápio do dia contendo o número do produto e seu respectivo valor.
 
-1001 | R$ 1.50
-
-1002 | R$ 2.50
-
-1003 | R$ 3.50
+// 1001 | R$ 1.50
+// 1002 | R$ 2.50
+// 1003 | R$ 3.50
 
 1004 | R$ 4.50
 
