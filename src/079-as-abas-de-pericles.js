60c3bef73199192f186034acecc9d7a95a0814e5
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-30 21:31:07 -0300

    Update 079-as-abas-de-pericles.js

--- src/079-as-abas-de-pericles.js
+++ src/079-as-abas-de-pericles.js
@@ -1,1 +1,16 @@
 
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+const str = lines.shift();
+
+let [initialTabs, actionsLength] = str.split(" ").map(item => Number(item));
+
+for (let act = 0; act < actionsLength; act++) {
+  const currentAction = lines.shift();
+
+  if (currentAction === "fechou") initialTabs++;
+  else initialTabs--;
+}
+
+console.log(initialTabs);
