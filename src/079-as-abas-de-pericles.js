fc7e6aa6e3ae54f0fbb58dd12536005454aadc63
Laks Castro <lakhsmycastro336@gmail.com>
2020-04-30 21:36:34 -0300

    Update 079-as-abas-de-pericles.js

--- src/079-as-abas-de-pericles.js
+++ src/079-as-abas-de-pericles.js
@@ -12,7 +12,7 @@
 // A entrada é iniciada por uma linha contendo dois números inteiros positivos, N e M (0 < N, M < 500), representando o número inicial de abas e o número de ações de Péricles. Cada linha subsequente contém uma ação (fechou ou clicou). Naturalmente, o número de abas é sempre maior ou igual a zero.
 
 // Saída
-A saída deve ser uma linha contendo o número final de abas.
+// A saída deve ser uma linha contendo o número final de abas.
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
