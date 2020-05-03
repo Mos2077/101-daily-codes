3ff1620c5e79945205c0423a38ef50b341e10d44
Laks Castro <lakhsmycastro336@gmail.com>
2020-05-02 23:33:45 -0300

    Update 081-selecao-em-vetor-i.js

--- src/081-selecao-em-vetor-i.js
+++ src/081-selecao-em-vetor-i.js
@@ -34,3 +34,28 @@ for (let i = 0; i < 100; i++) {
   // Logo, mostramos ele no console + seu index e com uma casa decimal
   console.log(`A[${i}] = ${current.toFixed(1)}`);
 }
+
+
+// Bom, adaptamos a saída desse exercício para que seja aleatória
+// Logo, sua saída do programa vai ser diferente dessa:
+// 'A[6] = 0.0'
+// 'A[17] = 10.0'
+// 'A[28] = 3.0'
+// 'A[30] = 6.0'
+// 'A[31] = 10.0'
+// 'A[32] = 8.0'
+// 'A[37] = 1.0'
+// 'A[50] = 4.0'
+// 'A[59] = 7.0'
+// 'A[78] = 9.0'
+// 'A[89] = 7.0'
+// 'A[94] = 3.0'
+
+// Porém ela vai compartilhar a mesma característica: ela vai imprimir somente números menores ou iguais a 10 
+// Obs: ou se você for um maníaco matemático programador, procure sobre números pseudoaleatórios, 
+// aí talvez, descobrindo a forma com que o computador/engine do javascript 
+// gera esses números e qual a expressão matemática por trás, 
+// você consiga manipular esses números e reproduzir uma saída igual a esta minha,
+// nunca pesquisei sobre, 
+// mas gosto da ideia de que é impossível gerar um número verdadeiramente aleatório através de e somente um algoritmo de computador,
+// no fundo sempre terá um script com uma fórmula matemática mirabolante, gerando resultados aparentemente aleatórios
