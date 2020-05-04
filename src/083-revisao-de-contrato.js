70922101fbb55f5ce31424f30401a91be7615e2c
Laks Castro <lakhsmycastro336@gmail.com>
2020-05-04 12:07:30 -0300

    Update 083-revisao-de-contrato.js

--- src/083-revisao-de-contrato.js
+++ src/083-revisao-de-contrato.js
@@ -19,12 +19,15 @@
 
 // Entrada
 // A entrada consiste de diversos casos de teste, cada um em uma linha. 
-// Cada linha contém dois inteiros D e N (1 ≤ D ≤ 9, 1 ≤ N < 10100 ), representando, respectivamente, o dígito que está apresentando problema na máquina e o número que foi negociado originalmente no contrato (que podem ser grande, pois Modernolândia tem sido acometida por hiperinflação nas últimas décadas).
+// Cada linha contém dois inteiros D e N (1 ≤ D ≤ 9, 1 ≤ N < 10100 ), representando, respectivamente, 
+// o dígito que está apresentando problema na máquina e o número que foi negociado originalmente no contrato 
+// (que podem ser grande, pois Modernolândia tem sido acometida por hiperinflação nas últimas décadas).
 
 // O ultimo caso de teste é seguido por uma linha que contém apenas dois zeros separados por espaços em branco.
 
 // Saída
-// Para cada caso de teste da entrada o seu programa deve imprimir uma linha contendo um único inteiro V, o valor numérico representado de fato no contrato.
+// Para cada caso de teste da entrada o seu programa deve imprimir uma linha contendo um único inteiro V, 
+// o valor numérico representado de fato no contrato.
 
 // CÓDIGO
 
