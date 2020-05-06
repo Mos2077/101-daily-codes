9806e89c12bb17b982787e7ff73c609684ec30c0
Laks Castro <lakhsmycastro336@gmail.com>
2020-05-05 23:01:45 -0300

    Update 084-matriz-123.js

--- src/084-matriz-123.js
+++ src/084-matriz-123.js
@@ -5,3 +5,37 @@ A entrada contém vários casos de teste e termina com EOF. Cada caso de teste �
 
 Saída
 Para cada N lido, apresente a saída conforme o exemplo fornecido.
+
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+while (true) {
+  const str = lines.shift();
+
+  if (!str) break;
+
+  const n = Number(str);
+
+  let oneI = 0;
+  let twoI = n - 1;
+
+  for (let x = 0; x < n; x++) {
+    let output = "";
+    for (let y = 0; y < n; y++) {
+      let current = "";
+
+      if (y === oneI) current = "1";
+
+      if (y === twoI) current = "2";
+
+      if (current === "") current = "3";
+
+      output += current;
+    }
+    console.log(output);
+
+    oneI++;
+    twoI--;
+  }
+}
