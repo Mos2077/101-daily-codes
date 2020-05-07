f9d0a41161f50c550d5c017dcc47c6b213388cee
Laks Castro <lakhsmycastro336@gmail.com>
2020-05-06 23:50:56 -0300

    Update 085-convertendo-para-hexadecimal.js

--- src/085-convertendo-para-hexadecimal.js
+++ src/085-convertendo-para-hexadecimal.js
@@ -1,3 +1,5 @@
+// EXERCÍCIO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
 // URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/1957
 
 // Os dados armazenados no computador estão em binário. 
@@ -12,6 +14,9 @@
 // Saída
 // A saída é o mesmo número V na base 16 em uma única linha (não esqueça do caractere de fim-de-linha).
 // Use letras maiúsculas, conforme os exemplos.
+
+
+// CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
 const lines = ["10"];
 
