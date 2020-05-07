5e8ca7e80076ed26b53c5b389a21df6d98a5a5d4
Laks Castro <lakhsmycastro336@gmail.com>
2020-05-07 00:39:33 -0300

    Update 086-fibonacci-em-vetor.js

--- src/086-fibonacci-em-vetor.js
+++ src/086-fibonacci-em-vetor.js
@@ -16,21 +16,36 @@
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
+// Função que retorna um array com a sequência inteira até o index N
 const fib = n => {
+  // Caso N seja zero, então o valor da sequência acaba no zero
   if (n === 0) return [0];
 
+  // Se o index for 1, então a sequência acaba no 1
   if (n === 1) return [0, 1];
 
+  // Agora se não for nem 0 nem 1, chame essa mesma função, passando N - 1
   const s = fib(n - 1);
+  
+  // E então adicione mais um termo que é a soma 2 últimos termos a
   s.push(s[s.length - 1] + s[s.length - 2]);
+  
+  // E retorna essa subsequência
+  // Isso vai dar um aspecto de loop em que o último valor retornado será todos os valoers anteriores
   return s;
 };
 
+// Agora na aplicação da função, temos:
+
+// Queremos pegar o index 4
 const n = 4;
 
+// Pegamos a sequência inteira passando N
 const sequence = fib(n);
 
+// Pegamos o valor final, o último valor da sequência
 const lastValue = sequence[sequence.length - 1];
 
+// E mostramos no console "Fib(N) = X"
 console.log(`Fib(${n}) = ${lastValue}`);
 
