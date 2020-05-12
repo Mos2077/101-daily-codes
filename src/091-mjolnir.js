e5e0663b8e1902200505f74b0fe630f774eb1cd0
Laks Castro <lakhsmycastro336@gmail.com>
2020-05-12 02:18:13 -0300

    Update 091-mjolnir.js

--- src/091-mjolnir.js
+++ src/091-mjolnir.js
@@ -24,14 +24,12 @@
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
-const input = require('fs').readFileSync('/dev/stdin', 'utf8');
-const lines = input.split('\n');
 
-const n = Number(lines.shift());
-
-for (let i = 0; i < n; i++) {
   const [name, newton] = lines.shift().split(" ");
 
   if (name === "Thor") console.log("Y");
   else console.log("N");
-}
+
+
+// Saída esperada para o a entrada "Thor":
+// 'Y'
