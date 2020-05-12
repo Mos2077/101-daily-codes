a16f0734a4aae5084530749cee87960f0849cb47
Laks Castro <lakhsmycastro336@gmail.com>
2020-05-12 02:15:39 -0300

    Update 091-mjolnir.js

--- src/091-mjolnir.js
+++ src/091-mjolnir.js
@@ -1,3 +1,5 @@
+// URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/1865
+
 // Odin created to Thor the most faithful and powerful possible weapon, Mjolnir hammer. Made of a special mystical ore called Uru and forged in the heart of a star by blacksmiths Gods of Asgard , Brokk and Eitri , blacksmiths legendary.
 
 // One day , Thor challenged his friends to see who could raise the Mjölnir .
