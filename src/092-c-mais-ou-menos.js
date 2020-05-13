c254656a5a80112cbb4dada66ef099083d4a26e2
Laks Castro <lakhsmycastro336@gmail.com>
2020-05-13 17:25:34 -0300

    Update 092-c-mais-ou-menos.js

--- src/092-c-mais-ou-menos.js
+++ src/092-c-mais-ou-menos.js
@@ -1,10 +1,9 @@
+// Ultimamente, diversas pessoas estão indo à Dra. Cláudia Café com Leite para saber se estão consumindo a quantidade recomendada diária de vitamina C. Isso tem a deixado exausta, e por isso ela lhe pediu para escrever um programa que, dado o consumo diário de alimentos ricos em vitamina C por uma pessoa, indique o quanto essa pessoa deve consumir a mais ou a menos para atingir o recomendado.
 
-Ultimamente, diversas pessoas estão indo à Dra. Cláudia Café com Leite para saber se estão consumindo a quantidade recomendada diária de vitamina C. Isso tem a deixado exausta, e por isso ela lhe pediu para escrever um programa que, dado o consumo diário de alimentos ricos em vitamina C por uma pessoa, indique o quanto essa pessoa deve consumir a mais ou a menos para atingir o recomendado.
+// Para tal, você poderá utilizar a tabela a seguir:
 
-Para tal, você poderá utilizar a tabela a seguir:
-
-Alimentos ricos em Vitamina C	Quantidade de Vitamina C
-suco de laranja	120 mg
+// Alimentos ricos em Vitamina C	Quantidade de Vitamina C
+// suco de laranja	120 mg
 morango fresco	85 mg
 mamao	85 mg
 goiaba vermelha	70 mg
