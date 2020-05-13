358c64d4895f32091469a447f7bcee0facc42634
Laks Castro <lakhsmycastro336@gmail.com>
2020-05-13 17:25:43 -0300

    Update 092-c-mais-ou-menos.js

--- src/092-c-mais-ou-menos.js
+++ src/092-c-mais-ou-menos.js
@@ -17,3 +17,35 @@ Cada caso de teste é composto um inteiro T (1 ≤ T ≤ 7) indicando que a pess
 
 Saída
 Para cada caso de teste (T), se o consumo ultrapassou o limite recomendado, imprima "Menos X mg", em que X representa a quantidade a menos a ser consumida para atingir o limite recomendado; se o consumo não atingiu o recomendado, imprima "Mais X mg", em que X representa a quantidade a mais para atingir o recomendado; se o consumo está dentro do intervalo recomendado, imprima "X mg", em que X representa a quantidade consumida diariamente pela pessoa.
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+const foods = {
+  "suco de laranja": 120,
+  "morango fresco": 85,
+  mamao: 85,
+  "goiaba vermelha": 70,
+  manga: 56,
+  laranja: 50,
+  brocolis: 34,
+};
+
+while (true) {
+  const t = Number(lines.shift());
+
+  if (!t) break;
+
+  let total = 0;
+
+  for (let i = 0; i < t; i++) {
+    let [qtd, ...name] = lines.shift().split(" ");
+    name = name.join(" ");
+
+    total += foods[name] * qtd;
+  }
+
+  if (total < 110) console.log("Mais " + (110 - total) + " mg");
+  else if (total > 130) console.log("Menos " + (total - 130) + " mg");
+  else console.log(total + " mg");
+}
