10ce9abb7cc1890cdfbd0f16730e8f8b1d353ed5
Laks Castro <lakhsmycastro336@gmail.com>
2020-05-15 00:20:03 -0300

    Update 094-tri-du.js

--- src/094-tri-du.js
+++ src/094-tri-du.js
@@ -18,8 +18,9 @@
 // A entrada consiste de uma ´unica linha que contém dois inteiros, A (1 ≤ A ≤ 13) e B (1 ≤ B ≤ 13) 
 // indicando os valores das duas primeiras cartas recebidas.
 
-Saída
-Seu programa deve produzir uma única linha com um inteiro representando o valor da carta que maximiza a probabilidade de o jogador ganhar a partida.
+// Saída
+// Seu programa deve produzir uma única linha com um inteiro representando o valor da carta que maximiza 
+// a probabilidade de o jogador ganhar a partida.
 
 
 // CÓDIGO <><><><>
