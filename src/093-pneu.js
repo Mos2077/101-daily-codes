3afdd79b2c40e4acccb0e46c3581885ee790dd79
Laks Castro <lakhsmycastro336@gmail.com>
2020-05-14 23:51:52 -0300

    Update 093-pneu.js

--- src/093-pneu.js
+++ src/093-pneu.js
@@ -23,10 +23,7 @@
 
 // CÓDIGO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
-const input = require('fs').readFileSync('/dev/stdin', 'utf8');
-const lines = input.split('\n').map(input => Number(input));
-
-const targetPressure = lines[0];
-const currrentPressure = lines[1];
+const targetPressure = 30;
+const currrentPressure = 20;
 
 console.log(targetPressure - currrentPressure);
