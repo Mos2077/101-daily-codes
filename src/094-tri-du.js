19aa6d2c9798175ab57a0aaecb0a5d249ca73e4d
Laks Castro <lakhsmycastro336@gmail.com>
2020-05-15 00:19:50 -0300

    Update 094-tri-du.js

--- src/094-tri-du.js
+++ src/094-tri-du.js
@@ -14,8 +14,9 @@
 // Sua tarefa é escrever um programa para determinar qual o valor da terceira carta que maximiza a
 // probabilidade de esse jogador ganhar o jogo.
 
-Entrada
-A entrada consiste de uma ´unica linha que contém dois inteiros, A (1 ≤ A ≤ 13) e B (1 ≤ B ≤ 13) indicando os valores das duas primeiras cartas recebidas.
+// Entrada
+// A entrada consiste de uma ´unica linha que contém dois inteiros, A (1 ≤ A ≤ 13) e B (1 ≤ B ≤ 13) 
+// indicando os valores das duas primeiras cartas recebidas.
 
 Saída
 Seu programa deve produzir uma única linha com um inteiro representando o valor da carta que maximiza a probabilidade de o jogador ganhar a partida.
