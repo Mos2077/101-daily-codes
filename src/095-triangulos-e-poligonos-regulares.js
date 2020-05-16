d0c7f0f8ad4a800df9787bcf60409fe16fae20d9
Laks Castro <lakhsmycastro336@gmail.com>
2020-05-16 11:21:20 -0300

    Update 095-triangulos-e-poligonos-regulares.js

--- src/095-triangulos-e-poligonos-regulares.js
+++ src/095-triangulos-e-poligonos-regulares.js
@@ -1,3 +1,18 @@
+O professor Rafael adora computação gráfica. Ele gosta muito de renderizar objetos, especialmente bonecas. Mas ele não gosta de desenhar polígonos regulares usando triângulos. Fazer isso consiste em compor um polígono regular usando apenas triângulos, de forma que os triângulos não se interceptem e se utilize o menor número possível de triângulos na composição. Veja um quadrado e um pentágono com os triângulos na figura.
+
+Poligonos
+
+// O professor Rafael pediu para você calcular qual o número mínimo de triângulos necessários para compor um polígono regular de N lados.
+
+// Obs: visite a URL do problema para ver ilustrações
+
+Entrada
+A entrada é composta por um número N (3 ≤ N ≤ 109), que indica o número de lados de um polígono regular.
+
+Saída
+A saída é o número mínimo de triângulos necessários para compor um polígono regular de N lados.
+
+
 const input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = input.split('\n');
 
