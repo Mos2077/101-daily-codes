3f74950ad8022c398c7763180b9792d5dd6ec32d
Laks Castro <lakhsmycastro336@gmail.com>
2020-05-18 17:53:15 -0300

    Update 097-tomadas.js

--- src/097-tomadas.js
+++ src/097-tomadas.js
@@ -1,5 +1,7 @@
 // EXERCÍCIO <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
+// URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/1930
+
 // Finalmente, o time da Universidade conseguiu a classificação para a Final Nacional da Maratona de Programação da SBC. 
 // Os três membros do time e o técnico estão ansiosos para bem representar a Universidade, e além de treinar muito, preparam 
 // com todos os detalhes a sua viagem a São Paulo, onde será realizada a Final Nacional.
