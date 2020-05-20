b962e90c646deb47627eb004fa86464319cccd8e
Laks Castro <lakhsmycastro336@gmail.com>
2020-05-20 02:02:52 -0300

    Update 099-telescopio.js

--- src/099-telescopio.js
+++ src/099-telescopio.js
@@ -29,7 +29,10 @@
 // e área de abertura de um telescópio, dizer quantas estrelas serão perceptíveis usando tal telescópio.
 
 // Entrada
-// A primeira linha da entrada terá um inteiro A (1 ≤ A ≤ 10.000) representando a área de abertura do telescópio (em milímetros quadrados) a ser considerado. A segunda linha possui um inteiro N (1 ≤ N ≤ 10.000) representando o número de estrelas a serem estudadas. As N linhas seguintes terão, cada uma, um inteiro F (1 ≤ F ≤ 20.000) representando o fluxo de fótons que cada uma das N estrelas emitem (em fótons por segundo por milímetro quadrado).
+// A primeira linha da entrada terá um inteiro A (1 ≤ A ≤ 10.000) representando a 
+// área de abertura do telescópio (em milímetros quadrados) a ser considerado. 
+// A segunda linha possui um inteiro N (1 ≤ N ≤ 10.000) representando o número 
+// de estrelas a serem estudadas. As N linhas seguintes terão, cada uma, um inteiro F (1 ≤ F ≤ 20.000) representando o fluxo de fótons que cada uma das N estrelas emitem (em fótons por segundo por milímetro quadrado).
 
 // Saída
 // Imprima um inteiro representando a quantidade de estrelas que serão percebidas ao se utilizar o telescópio em questão.
