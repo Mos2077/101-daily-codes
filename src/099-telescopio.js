20937d23e848b2661bb4e7b3fab07454d3995c5e
Laks Castro <lakhsmycastro336@gmail.com>
2020-05-20 01:56:23 -0300

    Update 099-telescopio.js

--- src/099-telescopio.js
+++ src/099-telescopio.js
@@ -13,3 +13,23 @@ A primeira linha da entrada terá um inteiro A (1 ≤ A ≤ 10.000) representand
 
 Saída
 Imprima um inteiro representando a quantidade de estrelas que serão percebidas ao se utilizar o telescópio em questão.
+
+
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.split('\n');
+
+const telescope = parseInt(lines.shift());
+
+const cases = parseInt(lines.shift());
+
+let total = 0;
+
+for (let cas = 0; cas < cases; cas++) {
+  const photons = lines[cas];
+
+  const result = photons * telescope;
+
+  if (result >= 40000000) total++;
+}
+
+console.log(total);
