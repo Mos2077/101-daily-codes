3970c8d175316b96f844b3f556b67fe5c5288321
Laks Castro <lakhsmycastro336@gmail.com>
2020-05-20 02:00:09 -0300

    Update 099-telescopio.js

--- src/099-telescopio.js
+++ src/099-telescopio.js
@@ -8,7 +8,10 @@
 
 // A maneira como os telescópios melhoram a nossa percepção dos astros no céu é aumentando 
 // a quantidade de luz captada que chega aos nossos olhos. Toda luz que entra pelos nossos olhos
-// entra por um orifício chamado pupila. Tal controla a quantidade de luz que entra nos olhos, aumentando o diâmetro quando o ambiente está escuro (e portanto precisamos obter mais luz para identificar os objetos) e diminuindo quando o ambiente está claro. Num ambiente muito escuro, a pupila pode atingir um diâmetro de 8 mm.
+// entra por um orifício chamado pupila. Tal controla a quantidade de luz que entra nos olhos, 
+// aumentando o diâmetro quando o ambiente está escuro (e portanto precisamos obter mais luz 
+// para identificar os objetos) e diminuindo quando o ambiente está claro. Num ambiente muito escuro,
+// a pupila pode atingir um diâmetro de 8 mm.
 
 // Cada objeto celeste (estrela, planeta, nebulosa, etc) emite uma quantidade de luz (fótons) que é homogeneamente distribuída quando chega na Terra. Por exemplo, a estrela A emite luz que pode ser captada a um fluxo de 40.000 fótons por segundo por milímetro quadrado. Isso é, a cada segundo, é possível captar 40.000 fótons provenientes da estrela A numa área de 1 mm 2. Ou seja, uma pupila de 10 mm 2 de área captaria 400.000 fótons provenientes da estrela A por segundo.
 
