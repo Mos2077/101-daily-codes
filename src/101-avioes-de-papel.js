aeac479dba93af550900422ff19aa04c84c767cc
Laks Castro <lakhsmycastro336@gmail.com>
2020-05-22 13:39:15 -0300

    Update 101-avioes-de-papel.js

--- src/101-avioes-de-papel.js
+++ src/101-avioes-de-papel.js
@@ -20,7 +20,8 @@
 // Entrada
 // A entrada contém um único conjunto de testes, que deve ser lido do dispositivo de entrada padrão (normalmente o teclado). 
 // O arquivo de entrada contém três números inteiros C (1 ≤ C ≤ 1000), P (1 ≤ P ≤ 1000) e F (1 ≤ F ≤ 1000) representando 
-// respectivamente o número de competidores, a quantidade de folhas de papel especial compradas pela Diretora e a quantidade de folhas de papel especial que cada competidor deve receber.
+// respectivamente o número de competidores, a quantidade de folhas de papel especial compradas pela Diretora e a quantidade
+// de folhas de papel especial que cada competidor deve receber.
 
 // Saída
 // Seu programa deve imprimir, na saída padrão, o caractere ‘S’ se a quantidade de folhas compradas pela Diretora é suficiente, 
